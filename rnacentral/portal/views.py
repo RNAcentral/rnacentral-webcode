7fe231999941caa52f3120bf40a36f1ed90b078b
Anton Petrov <apetrov@ebi.ac.uk>
2013-09-20 12:22:49 +0100

    Add basic RNA view

--- rnacentral/portal/views.py
+++ rnacentral/portal/views.py
@@ -2,6 +2,8 @@ from portal.models import Rna
 from rest_framework import viewsets
 from portal.serializers import RnaSerializer, XrefSerializer
 
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
 
 class RnaViewSet(viewsets.ReadOnlyModelViewSet):
     """
@@ -10,3 +12,15 @@ class RnaViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Rna.objects.defer('seq_long', 'seq_short').select_related().all()
     serializer_class = RnaSerializer
     paginate_by = 10
+
+
+def index(request):
+    return HttpResponse('Hello')
+
+
+def rna_view(request, upi):
+    try:
+        rna = Rna.objects.select_related().get(upi=upi)
+    except Rna.DoesNotExist:
+        raise Http404
+    return render(request, 'portal/rna_view.html', {'rna': rna})
--- rnacentral/rnacentral/urls.py
+++ rnacentral/rnacentral/urls.py
@@ -5,9 +5,13 @@ from portal import views
 router = routers.DefaultRouter()
 router.register(r'rnas', views.RnaViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns('',
-    url(r'^', include(router.urls)),
+    url(r'^$', 'portal.views.index'),
+
+    url(r'^rna/(?P<upi>\w+)$', 'portal.views.rna_view'),
+
+	# Wire up our API using automatic URL routing.
+	# Additionally, we include login URLs for the browseable API.
+    url(r'^api', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
