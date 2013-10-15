4d9ac3df8a78d889fa5b814b1d3e13427b7c3def
Anton Petrov <apetrov@ebi.ac.uk>
2013-10-15 16:50:05 +0100

    Turn off the reference serializer

--- rnacentral/portal/serializers.py
+++ rnacentral/portal/serializers.py
@@ -1,4 +1,4 @@
-from portal.models import Rna, Xref, Reference
+from portal.models import Rna, Xref#, Reference
 from rest_framework import serializers
 
 
@@ -9,17 +9,17 @@ class XrefSerializer(serializers.HyperlinkedModelSerializer):
         depth = 1
 
 
-class RefSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Reference
-        fields = ('authors', 'title', 'location', 'pubmed', 'doi', 'publisher', 'editors')
+# class RefSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Reference
+#         fields = ('authors', 'title', 'location', 'pubmed', 'doi', 'publisher', 'editors')
 
 
 class RnaSerializer(serializers.HyperlinkedModelSerializer):
     sequence = serializers.Field(source='get_sequence')
     xrefs = XrefSerializer(many=True)
-    refs = RefSerializer(many=True)
+    # refs = RefSerializer(many=True)
 
     class Meta:
         model = Rna
-        fields = ('upi', 'md5', 'sequence', 'xrefs', 'refs')
+        fields = ('upi', 'md5', 'sequence', 'xrefs') #, 'refs')
