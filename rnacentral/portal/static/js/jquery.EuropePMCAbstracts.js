a0533a964f85894b561162439e5d55fe0c81f522
Anton Petrov <apetrov@ebi.ac.uk>
2014-01-09 10:55:35 +0000

    Toggle show abstract button text

--- rnacentral/portal/static/js/jquery.EuropePMCAbstracts.js
+++ rnacentral/portal/static/js/jquery.EuropePMCAbstracts.js
@@ -37,12 +37,20 @@
         return abstractText;
     };
 
+    set_button_label = function($button, $abstractText) {
+        var button_label = $abstractText.is(':visible') ?
+                           $.fn.EuropePMCAbstracts.options.msg.hide_abstract :
+                           $.fn.EuropePMCAbstracts.options.msg.show_abstract;
+        $button.html(button_label);
+    };
+
     // main toggle function
     $.fn.toggleAbstract = function ( options ) {
 
       // setup
       var pubmed_id = this.data($.fn.EuropePMCAbstracts.options.pubmed_id_data_attribute);
       var $target = this.siblings($.fn.EuropePMCAbstracts.options.target_class);
+      var $this = $(this);
 
       // do nothing if there is no pubmed id
       if (!pubmed_id) {
@@ -52,13 +60,15 @@
 
       // abstract already loaded
       if ( $target.html().length > 0 ) {
-        $target.slideToggle();
-        return this;
+          $target.slideToggle(function(){
+              set_button_label($this, $target);
+          });
+          return this;
       }
 
       // loading the abstract for the first time
       $target.hide();
-      var query = 'ext_id:' + this.data('pubmed-id') + '&format=json&resulttype=core&callback=callback"';
+      var query = 'ext_id:' + pubmed_id + '&format=json&resulttype=core&callback=callback"';
 
       $.ajax({
         url: $.fn.EuropePMCAbstracts.options.europepmc_url + query,
@@ -68,11 +78,12 @@
       }).done(function(data){
         if ( is_valid_result(data) ) {
             show_message($target, get_abstract_text(data));
+            set_button_label($this, $target);
         } else {
             show_message($target, msg.error);
         }
       }).fail(function(){
-        show_message($target, msg.error);
+          show_message($target, msg.error);
       });
 
       return this; // for chaining
@@ -95,6 +106,8 @@
           target_class: '.abstract-text', // where to insert the abstract
           msg: {
               error: "Sorry, the abstract could not be retrieved",
+              show_abstract: "Show abstract",
+              hide_abstract: "Hide abstract",
           },
           europepmc_url: "http://www.ebi.ac.uk/europepmc/webservices/rest/search/query=",
     };
