402b567c313316647802f809ec9b0e7003b18775
Anton Petrov <apetrov@ebi.ac.uk>
2014-02-04 09:40:12 +0000

    Apply EBI license

--- rnacentral/portal/static/js/d3.speciesSunburst.js
+++ rnacentral/portal/static/js/d3.speciesSunburst.js
@@ -1,3 +1,16 @@
+/*
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 // Displays species sunburst based on the combined lineages from all xrefs.
 // Based on http://bl.ocks.org/mbostock/4348373
 // Text labels: http://blog.luzid.com/2013/extending-the-d3-zoomable-sunburst-with-labels/
