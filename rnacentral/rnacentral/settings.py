002c63f4bcc3e16423bfe701611a7351b5af4a09
Anton Petrov <apetrov@ebi.ac.uk>
2014-02-11 12:07:30 +0000

    Add a context processor for outputting the base url on the API documentation pages

--- rnacentral/rnacentral/settings.py
+++ rnacentral/rnacentral/settings.py
@@ -112,6 +112,17 @@ MIDDLEWARE_CLASSES = (
     'maintenancemode.middleware.MaintenanceModeMiddleware', # django-maintenance
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    "portal.context_processors.baseurl",
+)
+
 CACHE_MIDDLEWARE_ALIAS = 'default'
 CACHE_MIDDLEWARE_SECONDS = 600
 CACHE_MIDDLEWARE_KEY_PREFIX = ''
