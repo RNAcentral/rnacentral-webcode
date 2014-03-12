8741d457e9e31092fcc5243aa915f64e01e7a2f5
Anton Petrov <apetrov@ebi.ac.uk>
2014-03-12 16:31:47 +0000

    Move all js code from the rna_view into a separate file

---
+++ rnacentral/portal/static/js/rnaView.js
@@ -0,0 +1,133 @@
+/*
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+var rnaSequenceView = function(upi) {
+	this.upi = upi;
+};
+
+rnaSequenceView.prototype.initialize = function() {
+
+	obj = this;
+
+	load_xrefs();
+	activate_tooltips();
+	activate_literature_references();
+	activate_species_tree();
+	enable_show_species_tab_action();
+	enable_tab_hashtags();
+	enable_species_tree_scroll_action();
+
+	function load_xrefs() {
+      xref_loader = new xrefLoader(obj.upi);
+      xref_loader.load_xrefs();
+	};
+
+	function activate_tooltips() {
+      $('body').tooltip({
+          selector: '.help',
+          delay: { show: 200, hide: 100 }
+      });
+	};
+
+	function activate_literature_references() {
+      $(document).on('click', '.literature-refs-retrieve', function(){
+          var $this = $(this);
+          var accession = $this.data('accession');
+          var target = $this.siblings('.literature-refs-content');
+
+          insert_content = function(data) {
+              var source = $("#handlebars-literature-reference-tmpl").html();
+              var template = Handlebars.compile(source);
+              var wrapper  = {refs: data};
+              target.html(template(wrapper));
+          };
+
+          activate_abstract_buttons = function() {
+              $this.siblings().find('.abstract-btn').EuropePMCAbstracts({
+                'target_class': '.abstract-text',
+                'pubmed_id_data_attribute': 'pubmed-id',
+                'msg': {
+                  'show_abstract': 'Show abstract',
+                  'hide_abstract': 'Hide abstract',
+                }
+              });
+          };
+
+          if ( target.html().length > 0 ) {
+              target.slideToggle();
+          } else {
+            $.get('/api/v1/accession/' + accession + '/citations', function(data){
+                insert_content(data);
+                activate_abstract_buttons();
+                target.slideDown();
+            });
+          }
+      });
+	};
+
+	function activate_species_tree() {
+      var d3_species_tree = $('#d3-species-tree');
+      if(!document.createElement('svg').getAttributeNS){
+        d3_species_tree.html('Your browser does not support SVG');
+      } else {
+        $.ajax({
+          url: "/rna/" + obj.upi + "/lineage",
+          dataType: "json",
+          success: function(data) {
+            var tree = $('#d3-species-tree');
+            tree.hide().html('');
+            d3SpeciesTree(data, '#d3-species-tree');
+            tree.fadeIn();
+          },
+          error: function() {
+            var source = $("#handlebars-loading-error-tmpl").html();
+            var msg = Handlebars.compile(source);
+            d3_species_tree.hide().html(msg).fadeIn();
+          }
+        });
+      }
+    };
+
+    function enable_show_species_tab_action() {
+      // clicking the species link to view the Species tab
+      $("#show-species-tab").click(function(){
+        $('#tabs a[href="#species"]').tab('show');
+        window.location.hash = "#species";
+        return false;
+      });
+    };
+
+    function enable_tab_hashtags() {
+      // update the url hashtag when tabs are clicked
+      var hash = window.location.hash;
+      hash && $('#tabs a[href="' + hash + '"]').tab('show');
+      $('.nav-tabs a').click(function (e) {
+        var $this = $(this);
+        // only show the active tabs
+        if ( $this.attr('id') == 'overview' || $this.attr('id') == 'species' ) {
+           $this.tab('show');
+        }
+        var scrollmem = $('body').scrollTop();
+        window.location.hash = this.hash;
+        $('html,body').scrollTop(scrollmem);
+      });
+    };
+
+    function enable_species_tree_scroll_action() {
+      // scoll the species tree to the species level
+      $("#d3-species-scroll-tree").click(function(){
+        $('#d3-species-tree-tab').scrollLeft($('#d3-species-tree-tab svg').attr('width'));
+      });
+    };
+
+};
