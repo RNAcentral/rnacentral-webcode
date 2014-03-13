615ba88ee58e7a6ee4fa1462be334ef81e32ea5a
Anton Petrov <apetrov@ebi.ac.uk>
2014-03-13 11:45:01 +0000

    Export data in fasta format + minor refactoring

--- rnacentral/portal/management/commands/export.py
+++ rnacentral/portal/management/commands/export.py
@@ -12,7 +12,7 @@ limitations under the License.
 """
 
 from django.core.management.base import BaseCommand, CommandError
-from portal.models import Xref
+from portal.models import Xref, Rna
 from optparse import make_option
 import subprocess
 from cProfile import Profile
@@ -21,20 +21,21 @@ import os
 
 class Command(BaseCommand):
     """
-    Provide manage.py custom action for exporting genomic coordinates
+    Provide manage.py custom action for exporting RNAcentral data
     in several formats.
 
     Implemented in Django in order to reuse formatting routines from the REST API.
     Can be run on a cluster using eHive for parallelization.
 
     Usage:
+    python manage.py export --format fasta
     python manage.py export --format gff
     python manage.py export --format gff3
     python manage.py export --format bed --bedToBigBed=/path/to/bedToBigBed # also outputs BigBed
     python manage.py export --bedToBigBed=/path/to/bedToBigBed # all formats
     """
 
-    help = 'Export RNA sequences with known genomic coordinates in various formats' # show with --help command line option
+    help = 'Export RNAcentral data in various formats' # show with --help command line option
     option_list = BaseCommand.option_list + ( # add command line options
         make_option('--bedToBigBed',
             action='store',
@@ -45,7 +46,7 @@ class Command(BaseCommand):
             action='store',
             dest='format',
             default=False,
-            help='Output format (gff|gff3|bed). Omit for output in all formats.'),
+            help='Output format (gff|gff3|bed|fasta). Omit for output in all formats.'),
         make_option('--profile',
             default=False,
             help='Show cProfile information'),
@@ -61,6 +62,9 @@ class Command(BaseCommand):
         }
 
     def handle(self, *args, **options):
+        """
+        Main function called by django.
+        """
         if options['profile']:
             profiler = Profile()
             profiler.runcall(self._handle, *args, **options)
@@ -70,10 +74,12 @@ class Command(BaseCommand):
 
     def _handle(self, *args, **options):
         """
-        Main function called by django.
+        Main program. Separated from `handle` to enable Python profiling.
         """
+
         def _bedToBigBed_option_is_ok():
             """
+            Make sure that --bedToBigBed has been specified.
             """
             if not options['bedToBigBed']:
                 self.stderr.write('Specify path to bedToBigBed binary and fetchChromSizes.sh using --bedToBigBed')
@@ -83,10 +89,7 @@ class Command(BaseCommand):
                 return True
 
         if not options['format']:
-            self.export_gff(self.genomes['human'])
-            self.export_gff3(self.genomes['human'])
-            if _bedToBigBed_option_is_ok():
-                self.export_bed(self.genomes['human'])
+            self.export_everything()
         elif options['format'] == 'gff':
             self.export_gff(self.genomes['human'])
         elif options['format'] == 'gff3':
@@ -94,15 +97,52 @@ class Command(BaseCommand):
         elif options['format'] == 'bed':
             if _bedToBigBed_option_is_ok():
                 self.export_bed(self.genomes['human'])
+        elif options['format'] == 'fasta':
+            self.export_fasta()
         else:
             self.stderr.write('Incorrect output format')
-            return
 
     def get_vega_xrefs(self):
         """
         Get RNA sequences for output.
         """
         return Xref.objects.filter(db_id=5).select_related('accession', 'accession__assembly', 'accession__assembly__chromosome').all()
+
+    def gzip_file(self, filename):
+        """
+        Compress a given file using gzip, return the compressed file name.
+        """
+        gzipped_filename = '%s.gz' % filename
+        cmd = 'gzip < %s > %s' % (filename, gzipped_filename)
+        subprocess.call(cmd, shell=True)
+        self.stdout.write('File %s gzipped into %s' % (filename, gzipped_filename))
+        return gzipped_filename
+
+    def export_everything(self):
+        """
+        Export all data in all formats.
+        """
+        self.export_gff(self.genomes['human'])
+        self.export_gff3(self.genomes['human'])
+        if _bedToBigBed_option_is_ok():
+            self.export_bed(self.genomes['human'])
+        self.export_fasta()
+
+    def export_fasta(self):
+        """
+        Export all RNAcentral sequences in FASTA format.
+        Total runtime for 6M records: ~34m
+        """
+        filename = 'rnacentral.fasta'
+        rnas = Rna.objects.only('upi', 'seq_short', 'seq_long').\
+                           order_by('upi').\
+                           iterator()
+        f = open(filename, 'w')
+        for rna in rnas:
+            f.write(rna.get_sequence_fasta())
+        f.close()
+        self.gzip_file(filename)
+        self.stdout.write('Fasta export complete')
 
     def export_bed(self, genome):
         """
