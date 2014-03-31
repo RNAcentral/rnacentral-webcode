b75d2083e3aab3a31ff341dcb308478d542d875f
Anton Petrov <apetrov@ebi.ac.uk>
2014-03-31 09:58:25 +0100

    Move Metasearch API code into a separate file

---
+++ rnacentral/apiv1/metasearch.py
@@ -0,0 +1,54 @@
+"""
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+"""
+Docstrings of the classes exposed in urlpatters support markdown.
+"""
+
+from portal.models import Rna
+from rest_framework import generics
+from rest_framework import renderers
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.reverse import reverse
+from apiv1.serializers import RnaSearchSerializer
+import re
+import requests
+
+
+class MetaSearch(APIView):
+    """
+    """
+    permission_classes = (AllowAny,)
+    renderer_classes = (renderers.JSONRenderer,)
+
+    def get(self, request):
+        """
+        accession    sequence_md5
+        KF849944.1:14671..14739:tRNA 0026a4417938693f6af2993bc2920970
+        """
+        url = 'http://www.ebi.ac.uk/ena/data/warehouse/search?query=%22tax_eq(9606)%22&domain=noncoding&fields=sequence_md5&display=report&result=noncoding_update&sortfields=sequence_md5&offset=0&limit=100'
+        r = requests.get(url)
+        md5s = []
+        for i, line in enumerate(r.text.split('\n')):
+            if i == 0 or line == '':
+                continue
+            (accession, md5) = line.split('\t')
+            md5s.append(md5)
+        md5s = set(md5s)
+        rna = Rna.objects.filter(md5__in=md5s).all()
+
+        serializer = RnaSearchSerializer(rna)
+        return Response(serializer.data)
+
