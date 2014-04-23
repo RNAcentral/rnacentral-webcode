1c2739c7fd6b33148a97d028e0d32b4a5573139b
Anton Petrov <apetrov@ebi.ac.uk>
2014-04-23 14:39:10 +0100

    Bugfix for xml escaping

--- rnacentral/portal/management/commands/xml_exporters/rna2xml.py
+++ rnacentral/portal/management/commands/xml_exporters/rna2xml.py
@@ -185,7 +185,7 @@ class RnaXmlExporter(OracleConnection):
             if value: # organelle can be empty e.g.
                 text.append('<field name="{0}">{1}</field>'.format(field,
                     value))
-        return '\n'.join(escape(text))
+        return escape('\n'.join(text))
 
     def get_cross_references(self):
         """
