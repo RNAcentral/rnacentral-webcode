75573d9b0678e40dab25cc843b4e5547882e516c
Anton Petrov <apetrov@ebi.ac.uk>
2014-05-20 15:11:51 +0100

    Switch to the wwwdev EBeye endpoint

--- rnacentral/portal/static/js/search/metadata.search.js
+++ rnacentral/portal/static/js/search/metadata.search.js
@@ -50,7 +50,7 @@ rnaMetasearch.service('results', ['_', '$http', '$location', '$window', function
     var search_in_progress = false;
 
     var search_config = {
-        ebeye_base_url: 'http://ash-4.ebi.ac.uk:8080/ebisearch/ws/rest/rnacentral',
+        ebeye_base_url: 'http://wwwdev.ebi.ac.uk/ebisearch/ws/rest/rnacentral',
         rnacentral_base_url: get_base_url(),
         fields: ['description', 'active', 'length'],
         facetfields: ['expert_db', 'rna_type', 'TAXONOMY'], // will be displayed in this order
