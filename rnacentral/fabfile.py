50a35c112849f09315dfb64102968d5160817354
Anton Petrov <apetrov@ebi.ac.uk>
2014-06-11 12:40:18 +0100

    Rsync static files from test to production

--- rnacentral/fabfile.py
+++ rnacentral/fabfile.py
@@ -19,93 +19,116 @@ from fabric.api import *
 
 
 def rsync_git_repo():
-	"""
-	Use rsync to sync git repositories. This is required for django-compressor
-	to generate the same static files, which rely on file modification time.
-	"""
-	with lcd(env['rnacentral_site']):
-		this_dir = os.path.dirname(os.path.realpath(__file__))
-		parent_dir = os.path.abspath(os.path.join(this_dir, os.pardir))
-		parent_parent_dir = os.path.abspath(os.path.join(parent_dir, os.pardir))
-		cmd = 'rsync -av {src} {host}:{dst}'.format(
-			host=env.host, src=parent_dir, dst=parent_parent_dir)
-		local(cmd)
+    """
+    Use rsync to sync git repositories. This is required for django-compressor
+    to generate the same static files, which rely on file modification time.
+    """
+    with lcd(env['rnacentral_site']):
+        this_dir = os.path.dirname(os.path.realpath(__file__))
+        parent_dir = os.path.abspath(os.path.join(this_dir, os.pardir))
+        parent_parent_dir = os.path.abspath(os.path.join(parent_dir, os.pardir))
+        cmd = 'rsync -av {src} {host}:{dst}'.format(
+            host=env.host, src=parent_dir, dst=parent_parent_dir)
+        local(cmd)
 
 def git_updates():
-	"""
-	Perform git updates, but only on the test server because the production
-	servers must use rsync to preserve file modification time.
-	"""
-	with cd(env['rnacentral_site']):
-		git_branch = 'django'
-		# make sure we are on the right branch
-		run('git checkout %s' % git_branch)
-		# get latest changes
-		run('git pull')
-		# update git submodules
-		this_dir = os.path.dirname(os.path.realpath(__file__))
-		parent_dir = os.path.abspath(os.path.join(this_dir, os.pardir))
-		with cd(parent_dir):
-			run('git submodule update')
+    """
+    Perform git updates, but only on the test server because the production
+    servers must use rsync to preserve file modification time.
+    """
+    with cd(env['rnacentral_site']):
+        git_branch = 'django'
+        # make sure we are on the right branch
+        run('git checkout %s' % git_branch)
+        # get latest changes
+        run('git pull')
+        # update git submodules
+        this_dir = os.path.dirname(os.path.realpath(__file__))
+        parent_dir = os.path.abspath(os.path.join(this_dir, os.pardir))
+        with cd(parent_dir):
+            run('git submodule update')
 
-def django_updates():
-	"""
-	* activate virtual environment
-	* set Oracle variables
-	* install all python requirements
-	* move static files to the deployment location
-	"""
-	with cd(env['rnacentral_site']), prefix(env['activate']), prefix(env['ld_library_path']), prefix(env['oracle_home']):
-			run('pip install -r requirements.txt')
-			run('python manage.py collectstatic --noinput')
-			# TODO: django-compressor offline compression
+def install_django_requirements():
+    """
+    * activate virtual environment
+    * set Oracle variables
+    * install all python requirements
+    """
+    with cd(env['rnacentral_site']), prefix(env['activate']), prefix(env['ld_library_path']), prefix(env['oracle_home']):
+        run('pip install -r requirements.txt')
 
-def	flush_memcached():
-	"""
-	Delete all cached data.
-	"""
-	with cd(env['rnacentral_site']), settings(warn_only=True):
-			run('echo "flush_all" | nc -U rnacentral/memcached.sock')
+def rsync_static_files():
+    """
+    Rsync static files from the test to production.
+    This is done to synchronize modification times of css files.
+    """
+    with lcd(env['static_files']):
+        parent_dir = os.path.abspath(os.path.join(env['static_files'], os.pardir))
+        cmd = 'rsync -av {src} {host}:{dst}'.format(host=env.host,
+            src=env['static_files'], dst=parent_dir)
+        local(cmd)
+
+def collect_static_files():
+    """
+    * activate virtual environment
+    * set Oracle variables
+    * move static files to the deployment location
+    """
+    with cd(env['rnacentral_site']), prefix(env['activate']), prefix(env['ld_library_path']), prefix(env['oracle_home']):
+        run('python manage.py collectstatic --noinput')
+
+def flush_memcached():
+    """
+    Delete all cached data.
+    """
+    with cd(env['rnacentral_site']), settings(warn_only=True):
+            run('echo "flush_all" | nc -U rnacentral/memcached.sock')
 
 def restart_django(restart_url):
-	"""
-	Restart django process and visit the website.
-	"""
-	with cd(env['rnacentral_site']):
-		run('touch rnacentral/wsgi.py')
-		r = requests.get(restart_url)
-		if r.status_code != 200:
-		    print 'Error: Website cannot be reached'
+    """
+    Restart django process and visit the website.
+    """
+    with cd(env['rnacentral_site']):
+        run('touch rnacentral/wsgi.py')
+        r = requests.get(restart_url)
+        if r.status_code != 200:
+            print 'Error: Website cannot be reached'
 
 def deploy(restart_url="http://rnacentral.org"):
-	"""
-	Deploy to a server.
+    """
+    Deploy to a server.
 
-	fab.cfg template:
-	rnacentral_site=/path/to/manage.py
-	activate=source /path/to/virtualenvs/RNAcentral/bin/activate
-	ld_library_path=export LD_LIBRARY_PATH=/path/to/oracle/libraries/:$LD_LIBRARY_PATH
-	oracle_home=export ORACLE_HOME=/path/to/oracle/libraries/
+    fab.cfg template:
 
-	Usage:
-	fab -H user@server1,user@server2 -c /path/to/fab.cfg deploy:website_url
-	"""
-	if env.host == 'ves-hx-61':
-		git_updates() # will run only when deploying test servers
-	else:
-		rsync_git_repo() # will run only when deploying production servers
-	django_updates()
-	flush_memcached()
-	restart_django(restart_url)
+    rnacentral_site=/path/to/manage.py
+    activate=source /path/to/virtualenvs/RNAcentral/bin/activate
+    ld_library_path=export LD_LIBRARY_PATH=/path/to/oracle/libraries/:$LD_LIBRARY_PATH
+    oracle_home=export ORACLE_HOME=/path/to/oracle/libraries/
+    static_files=/path/to/static/files
+
+    Usage:
+    fab -H user@server1,user@server2 -c /path/to/fab.cfg deploy:website_url
+    """
+    if env.host == 'ves-hx-61':
+        # will run only when deploying to test servers
+        git_updates()
+        collect_static_files()
+    else:
+        # will run only when deploying to production servers
+        rsync_git_repo()
+        rsync_static_files()
+    install_django_requirements()
+    flush_memcached()
+    restart_django(restart_url)
 
 def test(base_url="http://localhost:8000/"):
-	"""
-	Single entry point for all tests.
+    """
+    Single entry point for all tests.
 
-	Usage:
-	fab test # will test localhost
-	fab test:http://test.rnacentral.org
-	"""
-	local('python apiv1/tests.py --base_url=%s' % base_url)
-	local('python portal/tests/selenium_tests.py --base_url %s --driver=phantomjs' % base_url)
-	local('python apiv1/search/sequence/tests.py --base_url %s' % base_url)
+    Usage:
+    fab test # will test localhost
+    fab test:http://test.rnacentral.org
+    """
+    local('python apiv1/tests.py --base_url=%s' % base_url)
+    local('python portal/tests/selenium_tests.py --base_url %s --driver=phantomjs' % base_url)
+    local('python apiv1/search/sequence/tests.py --base_url %s' % base_url)
