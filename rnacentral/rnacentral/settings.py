91b455954e5a8780db2def8247b4d99a08c9ed5d
Anton Petrov <apetrov@ebi.ac.uk>
2014-09-24 12:38:17 +0100

    Upgrade to Django 1.7
    * disabled some django-debug-toolbar panels
    * upgraded other dependencies that didn't work with 1.7

--- rnacentral/rnacentral/settings.py
+++ rnacentral/rnacentral/settings.py
@@ -256,13 +256,13 @@ REST_FRAMEWORK = {
 DEBUG_TOOLBAR_PANELS = (
     'debug_toolbar.panels.version.VersionDebugPanel',
     'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    # 'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
     'debug_toolbar.panels.headers.HeaderDebugPanel',
     'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
     'debug_toolbar.panels.template.TemplateDebugPanel',
     'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
+    # 'debug_toolbar.panels.signals.SignalDebugPanel',
+    # 'debug_toolbar.panels.logger.LoggingPanel',
 )
 
 # django-maintenance
