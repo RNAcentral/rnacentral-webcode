c1fdcc7db59e1a5af37379b86379393a561dcc99
Anton Petrov <apetrov@ebi.ac.uk>
2014-12-10 12:47:29 +0000

    Add a template script for environment variables
    the script should be copied to `env.sh` and modified as appropriate for
    the environment

---
+++ scripts/env_template.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+##############################################################################
+# Set environment variables
+##############################################################################
+
+# path to /rnacentral
+export RNACENTRAL_HOME=
+
+# path to directory with the redis-server binary
+export RNACENTRAL_REDIS=
+
+# path to directory with memcached binary
+export RNACENTRAL_MEMCACHED=
+
+# path to Oracle Instant Client libraries
+export ORACLE_HOME=
+export LD_LIBRARY_PATH=$ORACLE_HOME
+export DYLD_LIBRARY_PATH=$ORACLE_HOME
