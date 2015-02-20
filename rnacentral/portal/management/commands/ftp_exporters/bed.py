d1e70b6ff222198de1376c20ef2d4fe0c61b8aaf
Anton Petrov <apetrov@ebi.ac.uk>
2015-02-20 16:26:13 +0000

    Reorganise the `bed` file export script
    flatten deep nested functions

--- rnacentral/portal/management/commands/ftp_exporters/bed.py
+++ rnacentral/portal/management/commands/ftp_exporters/bed.py
@@ -24,7 +24,6 @@ import traceback
 class BedExporter(FtpBase):
     """
     Create bed and BigBed output files.
-    Total runtime for 21K records: ~14 min
     """
     def __init__(self, *args, **kwargs):
         """
@@ -46,6 +45,93 @@ class BedExporter(FtpBase):
         self.bedToBigBed = ''
         self.logger = logging.getLogger(__name__)
 
+    def fetch_chromosome_sizes(self, assembly_ucsc):
+        """
+        Fetch chromosome sizes for a specific genome.
+        Return the name of the output file.
+        """
+        filename = '{assembly_ucsc}.chrom.sizes'.format(assembly_ucsc=assembly_ucsc)
+        chrom_sizes_file = self.get_output_filename(filename, parent_dir=self.subdirectory)
+        cmd = '. %s/fetchChromSizes %s > %s' % (self.bedToBigBed, assembly_ucsc, chrom_sizes_file)
+        status = subprocess.call(cmd, shell=True)
+        if status == 0:
+            self.logger.info('Chromosome sizes fetched')
+            return chrom_sizes_file
+        else:
+            self.logger.critical(cmd)
+            self.logger.critical('Chromosome sizes file was not retrieved')
+            sys.exit(1)
+
+    def parse_chrom_sizes(self):
+        """
+        Store chromosome sizes in a dictionary.
+        """
+        self.chrom_sizes = dict()
+        with open(self.names['chrom_sizes'], 'r') as f:
+            reader = csv.reader(f, delimiter='\t')
+            for row in reader:
+                self.chrom_sizes[row[0]] = row[1]
+
+    def validate_chr_names(self, bed):
+        """
+        Ensure that all chr names appear in the chrom.sizes file.
+        """
+        if not bed:
+            return False
+        lines = bed.split('\n')
+        accepted = []
+        for line in lines:
+            if not line:
+                continue
+            words = line.split('\t')
+            if words[0] not in self.chrom_sizes:
+                # try with "chr"
+                words[0] = 'chr' + words[0]
+                if words[0] not in self.chrom_sizes:
+                    return False
+            accepted.append('\t'.join(words))
+        return '\n'.join(accepted) + '\n'
+
+    def export_unsorted_bed_data(self):
+        """
+        Export unsorted genomic coordinates in BED format.
+        """
+        counter = 0
+        accessions = self.get_xrefs_with_genomic_coordinates(taxid=self.genome['taxid'])
+        with open(self.names['bed_unsorted'], 'w') as f, \
+             open(self.names['example'], 'w') as example:
+            for accession in accessions:
+                text = Xref.objects.get(accession=accession, deleted='N').get_ucsc_bed()
+                text = self.validate_chr_names(text)
+                if text:
+                    f.write(text)
+                    counter += 1
+                    if counter < self.examples:
+                        example.write(text)
+                    if self.test and counter > self.test_entries:
+                        break
+        self.logger.info('Exported to file "%s"' % self.names['bed_unsorted'])
+
+    def bed_sort(self):
+        """
+        Sort bed file by chromosome then chromStart, as required by bedToBigBed.
+        """
+        cmd = "sort -k1,1 -k2,2n {0} > {1}".format(self.names['bed_unsorted'], self.names['bed_sorted'])
+        status = subprocess.call(cmd, shell=True)
+        if status == 0:
+            self.logger.info('Bed file sorted')
+        else:
+            self.logger.critical('Bed file could not be sorted')
+            sys.exit(1)
+
+    def remove_temp_files(self):
+        """
+        Remove temporary files.
+        """
+        os.remove(self.names['bed_sorted'])
+        os.remove(self.names['bed_unsorted'])
+        os.remove(self.names['chrom_sizes'])
+
     def export(self, genome=None, bedToBigBed=''):
         """
         Main export function.
@@ -58,130 +144,39 @@ class BedExporter(FtpBase):
         self.genome['species'] = self.genome['species'].replace(' ','_')
         self.bedToBigBed = bedToBigBed
 
-        def fetch_chromosome_sizes():
-            """
-            Fetch chromosome sizes for a specific genome and return the name of the output file.
-            """
-            filename = '{assembly_ucsc}.chrom.sizes'.format(assembly_ucsc=self.genome['assembly_ucsc'])
-            chrom_sizes_file = self.get_output_filename(filename, parent_dir=self.subdirectory)
-            cmd = '. %s/fetchChromSizes %s > %s' % (self.bedToBigBed, self.genome['assembly_ucsc'], chrom_sizes_file)
-            status = subprocess.call(cmd, shell=True)
-            if status == 0:
-                self.logger.info('Chromosome sizes fetched')
-                return chrom_sizes_file
-            else:
-                self.logger.critical(cmd)
-                self.logger.critical('Chromosome sizes file was not retrieved')
-                sys.exit(1)
+        self.names = self.name_templates.copy()
+        self.names['chrom_sizes'] = self.fetch_chromosome_sizes(self.genome['assembly_ucsc'])
+        for key, value in self.names.iteritems():
+            self.names[key] = value.format(**self.genome)
 
-        def initialize_names():
-            """
-            Replace placeholders in `name_templates` with self.genome
-            """
-            self.names = self.name_templates.copy()
-            self.names['chrom_sizes'] = fetch_chromosome_sizes()
-            for key, value in self.names.iteritems():
-                self.names[key] = value.format(**self.genome)
-
-        def parse_chrom_sizes():
-            """
-            Store chromosome sizes in a dictionary.
-            """
-            self.chrom_sizes = dict()
-            with open(self.names['chrom_sizes'], 'r') as f:
-                reader = csv.reader(f, delimiter='\t')
-                for row in reader:
-                    self.chrom_sizes[row[0]] = row[1]
-
-        initialize_names()
-        parse_chrom_sizes()
+        self.parse_chrom_sizes()
         try:
-            self.export_bed_data()
+            self.export_bed()
         except:
             self.logger.error(traceback.format_exc(sys.exc_info()))
             self.logger.critical('Error: aborting Bed and BigBed export')
             sys.exit(1)
 
-    def export_bed_data(self):
+    def export_bed(self):
         """
         Export the data in sorted BED format.
         """
-        def export_unsorted_bed_data():
-            """
-            Export unsorted genomic coordinates in BED format.
-            """
-            def validate_chr_names(bed_file):
-                """
-                Ensure that all chr names appear in the chrom.sizes file.
-                """
-                if not bed_file:
-                    return False
-                lines = bed_file.split('\n')
-                accepted = []
-                for line in lines:
-                    if not line:
-                        continue
-                    words = line.split('\t')
-                    if words[0] not in self.chrom_sizes:
-                        # try with "chr"
-                        words[0] = 'chr' + words[0]
-                        if words[0] not in self.chrom_sizes:
-                            return False
-                    accepted.append('\t'.join(words))
-                return '\n'.join(accepted) + '\n'
-
-            f = open(self.names['bed_unsorted'], 'w')
-            example = open(self.names['example'], 'w')
-            counter = 0
-            for accession in self.get_xrefs_with_genomic_coordinates(taxid=self.genome['taxid']):
-                text = Xref.objects.get(accession=accession, deleted='N').get_ucsc_bed()
-                text = validate_chr_names(text)
-                if text:
-                    f.write(text)
-                    counter += 1
-                    if counter < self.examples:
-                        example.write(text)
-                    if self.test and counter > self.test_entries:
-                        break
-            f.close()
-            example.close()
-            self.logger.info('Exported to file "%s"' % self.names['bed_unsorted'])
-
-        def bed_sort():
-            """
-            Sort bed file by chromosome then chromStart, as required by bedToBigBed.
-            """
-            cmd = "sort -k1,1 -k2,2n {0} > {1}".format(self.names['bed_unsorted'], self.names['bed_sorted'])
-            status = subprocess.call(cmd, shell=True)
-            if status == 0:
-                self.logger.info('Bed file sorted')
-            else:
-                self.logger.critical('Bed file could not be sorted')
-                sys.exit(1)
-
-        def remove_temp_files():
-            """
-            Remove temporary files.
-            """
-            os.remove(self.names['bed_sorted'])
-            os.remove(self.names['bed_unsorted'])
-            os.remove(self.names['chrom_sizes'])
-
         self.logger.info('Exporting bed and BigBed')
-        export_unsorted_bed_data()
-        bed_sort()
+        self.export_unsorted_bed_data()
+        self.bed_sort()
         if self.bedToBigBed:
             self.export_big_bed()
         self.gzip_file(self.names['bed_sorted'])
-        remove_temp_files()
+        self.remove_temp_files()
         self.logger.info('Bed and BigBed export complete')
 
     def export_big_bed(self):
         """
         Run bedToBigBed and produce the output.
         """
-        print self.names['chrom_sizes']
-        cmd = '%s/bedToBigBed %s %s %s' % (self.bedToBigBed, self.names['bed_sorted'], self.names['chrom_sizes'], self.names['big_bed'])
+        self.logger.info(self.names['chrom_sizes'])
+        cmd = '%s/bedToBigBed %s %s %s' % (self.bedToBigBed, \
+            self.names['bed_sorted'], self.names['chrom_sizes'], self.names['big_bed'])
         status = subprocess.call(cmd, shell=True)
         if status == 0:
             self.logger.info('BigBed file created')
