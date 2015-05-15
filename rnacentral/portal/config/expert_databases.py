1db098d2bb52aa6e76f306bb4b6d0c076e51c43b
Anton Petrov <apetrov@ebi.ac.uk>
2015-05-15 11:32:04 +0100

    Shorten WormBase description

--- rnacentral/portal/config/expert_databases.py
+++ rnacentral/portal/config/expert_databases.py
@@ -531,7 +531,7 @@ expert_dbs = [
         'name': 'WormBase',
         'label': 'wormbase',
         'url': 'http://www.wormbase.org/',
-        'description': "curates, stores and displays genomic and genetic data about nematodes with primary emphasis on <em>C. elegans</em> and related <em>Caenorhabditis</em> nematodes",
+        'description': "curates, stores and displays genomic and genetic data about nematodes with primary emphasis on <em>C. elegans</em> and related nematodes",
         'abbreviation': '',
         'examples': ['URS00001418F1'],
         'references': [
