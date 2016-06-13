bd083ea0689a329c4c8e3803fc21c1eefa15a42b
Anton Petrov <apetrov@ebi.ac.uk>
2016-06-13 12:34:17 +0100

    Make sure STATIC_ROOT is set

--- rnacentral/rnacentral/settings.py
+++ rnacentral/rnacentral/settings.py
@@ -25,7 +25,6 @@ TEMPLATE_DEBUG = DEBUG
 
 # project root directory
 PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # pylint: disable=C0301
-STATIC_ROOT = os.path.dirname(os.path.join(PROJECT_PATH, 'static'))
 
 ADMINS = (
     ('RNAcentral Team', ''.join(['rnacentral', '@', 'gmail.com'])),
@@ -87,7 +86,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.dirname(os.path.join(PROJECT_PATH, 'static'))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
