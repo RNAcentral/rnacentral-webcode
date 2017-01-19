c4bfeb55d6dc3c8a39202851b110bc356a55a2c6
Blake Sweeney <bsweeney@ebi.ac.uk>
2017-01-19 09:50:28 +0000

    Add rna_type to RnaPrecomputed
    This has been added by a migration so it should be reflected in the
    model.

--- rnacentral/portal/models.py
+++ rnacentral/portal/models.py
@@ -383,6 +383,7 @@ class RnaPrecomputed(models.Model):
     upi = models.ForeignKey('Rna', db_column='upi', to_field='upi', related_name='precomputed')
     taxid = models.IntegerField(db_index=True, null=True)
     description = models.CharField(max_length=250)
+    rna_type = models.CharField(max_length=250)
 
     class Meta:
         db_table = 'rnc_rna_precomputed'
