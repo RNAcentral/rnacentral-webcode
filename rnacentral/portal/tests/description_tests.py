c4ad2462f5a81540e0381b2929d83ae8cc949584
Blake Sweeney <bsweeney@ebi.ac.uk>
2017-01-19 12:15:20 +0000

    Add more description tests
    I think I have covered many of the major cases of issues I see. I know
    some of these examples are redundant, but adding them may be useful.
    More browsing seems to show all the same issues. Once these are tested
    fixed and deployed I may find a new set of issues, but for now these
    should cover everything.

--- rnacentral/portal/tests/description_tests.py
+++ rnacentral/portal/tests/description_tests.py
@@ -130,3 +130,71 @@ class MouseDescriptionTests(GenericDescriptionTest):
             'Mus musculus Small Cajal body-specific RNA 6',
             'URS0000653D5F', 
             taxid=10090)
+
+
+class CattleDescriptionTests(GenericDescriptionTest):
+    def test_likes_name_with_precursor(self):
+        self.assertDescriptionIs(
+            'Bos taurus (cattle) microRNA 431 precursor.',
+            'URS00007150F8',
+            taxid=9913)
+
+    def test_can_specify_the_trna(self):
+        self.assertDescriptionIs(
+            'transfer RNA-Trp from Bos taurus (PDB 2AKE, chain B)' ,
+            'URS0000669B12',
+            taxid=9913)
+
+    def test_will_use_mirbase_for_precusors(self):
+        self.assertDescriptionIs(
+            'Bos taurus (cattle) microRNA bta-mir-497 precursor',
+            'URS00006D80BC',
+            taxid=9913)
+
+        self.assertDescriptionIs(
+            'Bos taurus (cattle) microRNA bta-mir-10a precursor',
+            'URS000075CF25',
+            taxid=9913)
+
+    def test_likes_refseq_over_ena(self):
+        self.assertDescriptionIs(
+            'Bos taurus telomerase RNA component (TERC), telomerase RNA.',
+            'URS00003EBD9A',
+            taxid=9913)
+
+
+class WormTests(GenericDescriptionTest):
+    def test_likes_wormbase(self):
+        self.assertDescriptionIs(
+            'Caenorhabditis elegans Unclassified non-coding RNA linc-125',
+            'URS00005511ED',
+            taxid=6239)
+
+    # NOTE: This one is questionable as people may like wormbase more if they
+    # are used to it. I don't think it is as nice a name (is 26S large in all
+    # organisms I don't know) but this my choice for now.
+    def test_likes_silva_for_rrna_over_wormbase(self):
+        self.assertDescriptionIs(
+            'Caenorhabditis elegans large subunit ribosomal RNA (26S)',
+            'URS00004FB44B',
+            taxid=6239)
+
+    # NOTE: this may be questionable as there is a fairly specific looking name
+    # from wormbase, but the one from mirbase looks better to me. Need to see
+    # what people in the worm/miRNA field like.
+    def test_it_likes_mirbase_over_wormbase(self):
+        self.assertDescriptionIs(
+            'Caenorhabditis elegans microRNA cel-miR-229-5p',
+            'URS0000466DE6',
+            taxid=6239)
+
+    def test_likes_wormbase_over_rfam(self):
+        self.assertDescriptionIs(
+            'Caenorhabditis elegans tRNA-Undet',
+            'URS00006DC8B9',
+            taxid=6239)
+
+        self.assertDescriptionIs(
+            'Caenorhabditis elegans tRNA-His',
+            'URS000069D7FA',
+            taxid=6239)
