397c278678baf052e1e46c8c9c301ae2e3826fe7
Boris A. Burkov <vasjaforutube@gmail.com>
2017-02-07 17:28:08 +0000

    #94 Cleanup.

--- rnacentral/portal/static/js/genoverse.controller.js
+++ rnacentral/portal/static/js/genoverse.controller.js
@@ -24,8 +24,6 @@ angular.module('rnacentralApp').controller('GenoverseGenomeBrowser', ['$scope', 
     $scope.chromosome = chromosome;
     $scope.start = start;
     $scope.end = end;
-    $scope.strand = 1;
-    $scope.description = '';
 
     // handle copy to clipboard button
     new Clipboard('#copy-genome-location', {
@@ -56,64 +54,6 @@ angular.module('rnacentralApp').controller('GenoverseGenomeBrowser', ['$scope', 
         $location.search({chromosome: $scope.chromosome, start: $scope.start, end: $scope.end});
     }
 
-    /**
-     * Synchronize coordinates in Genoverse, in address bar, in form inputs and in the text line
-     * above genoverse genome browser.
-     *
-     * We need to find out, which one was actually changed by the user since last run and modify
-     * the others.
-     */
-    function updateCoordinates() {
-        // update form start/end inputs
-        start.val(browser.start);
-        end.val(browser.end);
-
-        // update location string right above genoverse
-        var species = $('#genomic-species-select');
-        text = '<em>' + species.val() + '</em>' + ' ' + browser.chr + ':' + browser.start + '-' + browser.end;
-        if ($('#genomic-location').html() != text) {
-            $('#genomic-location').html(text);
-        }
-
-        // update get param in browser, if they changed
-        var searchParams = "chromosome=" + browser.chr;
-        searchParams = searchParams.concat("&start=" + browser.start);
-        searchParams = searchParams.concat("&end=" + browser.end);
-
-        if (document.location.search != searchParams) {
-            history.pushState({}, document.title, document.location.pathname + "?" + searchParams);
-        }
-
-        // update url if species changed
-        var species = species.val().replace(/ /g, '-');
-
-        var pathname = document.location.pathname;
-        var hasTrailingSlash = false;
-        if (document.location.pathname.substr(-1) === '/') {
-            hasTrailingSlash = true;
-            pathname = document.location.pathname.substr(0, document.location.pathname.length - 1);
-        }
-
-        if (document.location.pathname.search(/genome-browser\/.+/) == -1) { // if url is like genome-browser/smth.
-            var newPathname = pathname + "/" + species;
-            if (hasTrailingSlash) { newPathname = newPathname + "/"; }
-            history.pushState({}, document.title, newPathname + "?" + searchParams);
-        }
-        else { // if url is like /genome-browser/homo-sapiens
-            var pathElements = pathname.split("/");
-            var urlBeginning = pathElements.slice(0, pathElements.length - 1);
-            var urlEnding = pathElements.slice(-1)[0];
-
-            if (urlEnding != species) {
-                urlBeginning.push(species);
-                var newPathname = urlBeginning.join("/");
-                if (hasTrailingSlash) {
-                    newPathname = newPathname + "/";
-                }
-                history.pushState({}, document.title, newPathname + "?" + searchParams);
-            }
-        }
-    }
 }]);
 
 })();
