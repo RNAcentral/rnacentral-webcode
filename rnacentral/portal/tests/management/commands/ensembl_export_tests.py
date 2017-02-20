e98fb22b76ba4bf98115627eca1eb3f16b2248da
Blake Sweeney <bsweeney@ebi.ac.uk>
2017-02-20 15:31:43 +0000

    Improve tests
    This actually implements several tests and adds a query limit and time
    limit constraint. This process should be very fast (it can be a single
    database query to fetch all sequences) so we add constraints that
    reflect that.

--- rnacentral/portal/tests/management/commands/ensembl_export_tests.py
+++ rnacentral/portal/tests/management/commands/ensembl_export_tests.py
@@ -1,56 +1,64 @@
-import unittest
+import itertools as it
 
 from django.test import SimpleTestCase
 
 from portal.management.commands.ensembl_export import Exporter
-# from portal.management.commands.ensembl_export import SimpleXref
-# from portal.management.commands.ensembl_export import SimpleSequence
+from portal.management.commands.ensembl_export import SimpleXref
+from portal.management.commands.ensembl_export import SimpleSequence
+
+from django_performance_testing.queries import QueryBatchLimit
+from django_performance_testing.timing import TimeLimit
 
 
 class BasicTest(SimpleTestCase):
 
-    @unittest.skip("No test data yet")
+    @property
+    def data(self):
+        exporter = Exporter(destination='test/files', test=True)
+        structured = it.imap(exporter.structure_data, exporter.get_data())
+        return list(structured)
+
     def test_it_fetches_all_sequences(self):
-        pass
+        assert len(self.data) == 32
 
-    @unittest.skip("No test data yet")
-    def test_it_sets_xref_id_to_external_id(self):
-        pass
-
-    @unittest.skip("No test data yet")
-    def test_it_sets_xref_database_to_display_name(self):
-        pass
-
-    @unittest.skip("No test data yet")
-    def test_it_fetches_all_xrefs(self):
-        pass
-
-    @unittest.skip("No test data yet")
-    def test_it_correctly_groups_all_xrefs(self):
-        pass
-
-    @unittest.skip("No test data yet")
-    def test_it_sets_correct_rnacentral_id(self):
-        self.assertEquals(self.data[0].rnacentral_id, '')
-
-    @unittest.skip("No test data yet")
-    def test_it_gets_sequence_when_short(self):
-        pass
-
-    @unittest.skip("No test data yet")
-    def test_it_gets_sequence_when_long(self):
-        pass
-
+    def test_it_builds_correct_entry(self):
+        with QueryBatchLimit(write=0, read=1):
+            with TimeLimit(total=1):
+                assert self.data[1] == SimpleSequence(
+                    upi='URS0000000004',
+                    taxon_id=77133,
+                    description='uncultured bacterium partial 16S ribosomal RNA',
+                    rna_type='',
+                    seq_short='CACGCAGGCGGTTCTGCGCGTTTCGAGTGACAGCGGGCGGCTTACCTGCCCGAGTATTCGAAAGACGGTAGGACTTGAGGGCCAGAGAGGGACACGGAATTCCGGGTGGAGTGGTGAAATGCGTAGAGATCCGGAGGAACACCGAAGGCGAAGGCAGTGTCCTGGCTGGTGACTGACGCTGAGGTGCGAAAGCCGAGGGGAGCGAACGGGATTAGATACCCCGGTAGTCCTGGCCGTAAACGATGACCACCAGGTGTGGGAGGTATCGACCCCTTCGTGCCGGAGTCAACACACTAAGTGGTCCGCCTGGGGATACGGTCGCAAGATTAAAA',
+                    seq_long='',
+                    md5='030c78be0f492872b95219d172e0c658',
+                    xrefs=[]
+                )
 
 class PdbeTest(SimpleTestCase):
-    def setUp(self):
+
+    @property
+    def data(self):
         self.exporter = Exporter('somewhere.json')
         raw = self.exporter.get_data(
             accession__database='PDBE',
             accession__external_id='1FJG',
             accession__optional_id='A')
-        self.data = self.exporter.structure_data(next(raw))
+        return self.exporter.structure_data(next(raw))
 
     def test_it_sets_id_to_pdb_and_chain(self):
-        self.assertEquals(len(self.data.xrefs), 1)
-        self.assertEqual(self.data.xrefs[0].id, '1FJG_A')
+        with QueryBatchLimit(write=0, read=1):
+            with TimeLimit(total=1):
+                data = self.data
+                assert data == SimpleSequence(
+                    upi='URS000080E226',
+                    taxon_id=274,
+                    description=u'16S rRNA from Thermus thermophilus (PDB 4YZV, chain XA)',
+                    rna_type='rRNA',
+                    seq_short='TTTGTTGGAGAGTTTGATCCTGGCTCAGGGTGAACGCTGGCGGCGTGCCTAAGACATGCAAGTCGTGCGGGCCGCGGGGTTTTACTCCGTGGTCAGCGGCGGACGGGTGAGTAACGCGTGGGTGACCTACCCGGAAGAGGGGGACAACCCGGGGAAACTCGGGCTAATCCCCCATGTGGACCCGCCCCTTGGGGTGTGTCCAAAGGGCTTTGCCCGCTTCCGGATGGGCCCGCGTCCCATCAGCTAGTTGGTGGGGTAATGGCCCACCAAGGCGACGACGGGTAGCCGGTCTGAGAGGATGGCCGGCCACAGGGGCACTGAGACACGGGCCCCACTCCTACGGGAGGCAGCAGTTAGGAATCTTCCGCAATGGGCGCAAGCCTGACGGAGCGACGCCGCTTGGAGGAAGAAGCCCTTCGGGGTGTAAACTCCTGAACCCGGGACGAAACCCCCGACGAGGGGACTGACGGTACCGGGGTAATAGCGCCGGCCAACTCCGTGCCAGCAGCCGCGGTAATACGGAGGGCGCGAGCGTTACCCGGATTCACTGGGCGTAAAGGGCGTGTAGGCGGCCTGGGGCGTCCCATGTGAAAGACCACGGCTCAACCGTGGGGGAGCGTGGGATACGCTCAGGCTAGACGGTGGGAGAGGGTGGTGGAATTCCCGGAGTAGCGGTGAAATGCGCAGATACCGGGAGGAACGCCGATGGCGAAGGCAGCCACCTGGTCCACCCGTGACGCTGAGGCGCGAAAGCGTGGGGAGCAAACCGGATTAGATACCCGGGTAGTCCACGCCCTAAACGATGCGCGCTAGGTCTCTGGGTCTCCTGGGGGCCGAAGCTAACGCGTTAAGCGCGCCGCCTGGGGAGTACGGCCGCAAGGCTGAAACTCAAAGGAATTGACGGGGGCCCGCACAAGCGGTGGAGCATGTGGTTTAATTCGAAGCAACGCGAAGAACCTTACCAGGCCTTGACATGCTAGGGAACCCGGGTGAAAGCCTGGGGTGCCCCGCGAGGGGAGCCCTAGCACAGGTGCTGCATGGCCGTCGTCAGCTCGTGCCGTGAGGTGTTGGGTTAAGTCCCGCAACGAGCGCAACCCCCGCCGTTAGTTGCCAGCGGTTCGGCCGGGCACTCTAACGGGACTGCCCGCGAAAGCGGGAGGAAGGAGGGGACGACGTCTGGTCAGCATGGCCCTTACGGCCTGGGCGACACACGTGCTACAATGCCCACTACAAAGCGATGCCACCCGGCAACGGGGAGCTAATCGCAAAAAGGTGGGCCCAGTTCGGATTGGGGTCTGCAACCCGACCCCATGAAGCCGGAATCGCTAGTAATCGCGGATCAGCCATGCCGCGGTGAATACGTTCCCGGGCCTTGTACACACCGCCCGTCACGCCATGGGAGCGGGCTCTACCCGAAGTCGCCGGGAGCCTACGGGCAGGCGCCGAGGGTAGGGCCCGTGACTGGGGCGAAGTCGTAACAAGGTAGCTGTACCGGAAGGTGCGGCTGGATCACCTCCTTTCT',
+                    seq_long='',
+                    md5='8c29ac94fb1a187b14036d4f9cbc9d83',
+                    xrefs=[
+                        SimpleXref(external_id=u'1FJG', optional_id=u'A', database=u'PDBe'),
+                    ])
+                assert data.xrefs[0].id == '1FJG_A'
