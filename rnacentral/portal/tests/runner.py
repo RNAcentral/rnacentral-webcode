e4b066fc133b080273012b105409a49d02e81a14
Blake Sweeney <bsweeney@ebi.ac.uk>
2017-03-06 13:50:40 +0000

    Add small stackoverflow link
    This will help give some context on what this code does.

--- rnacentral/portal/tests/runner.py
+++ rnacentral/portal/tests/runner.py
@@ -8,6 +8,11 @@ class FixedRunner(DiscoverRunner):
     attempt to modify the database turned into no-ops. Also this will always
     use the --keepdb option so as to ensure the database is not modifed and
     make the command line a bit cleaner.
+
+    See:
+    http://stackoverflow.com/questions/6250353/django-test-to-use-existing-database/42581239
+
+    for some context on what this does.
     """
 
     def _fixture_setup(self, *args, **kwargs):
