2419e44877279662d87feb6a07800b53a3694165
Boris A. Burkov <vasjaforutube@gmail.com>
2017-04-12 15:01:01 +0100

    #110 Bugfixes in taxonomyComponent.

--- rnacentral/portal/static/js/sequence.module.js
+++ rnacentral/portal/static/js/sequence.module.js
@@ -48,36 +48,33 @@ var taxonomyComponent = {
     controller: ['$http', '$interpolate',  function($http, $interpolate) {
         var ctrl = this;
 
-        var d3_species_tree = $('#d3-species-tree');
+        ctrl.$onInit =function() {
+            var d3_species_tree = $('#d3-species-tree');
 
-        if (!document.createElement('svg').getAttributeNS) {
-            d3_species_tree.html('Your browser does not support SVG');
-        }
-        else {
-            $http({
-                url: $interpolate("/rna/{{upi}}/lineage")({ upi: ctrl.upi }),
-                method: 'GET'
-            }).then(
-                function(response) {
-                    console.log(response);
-                    ctrl.response = response;
-                    d3SpeciesTree(response.data, ctrl.upi, '#d3-species-tree');
-                },
-                function(response) {
-                    console.log(response);
-                    ctrl.response = response;
-                }
-            )
-        }
+            if (!document.createElement('svg').getAttributeNS) {
+                d3_species_tree.html('Your browser does not support SVG');
+            }
+            else {
+                $http({
+                    url: $interpolate("/rna/{{upi}}/lineage")({ upi: ctrl.upi }),
+                    method: 'GET'
+                }).then(
+                    function(response) {
+                        ctrl.response = response;
+                        d3SpeciesTree(response.data, ctrl.upi, '#d3-species-tree');
+                    },
+                    function(response) {
+                        ctrl.response = response;
+                    }
+                )
+            }
+        };
     }],
     template: '<div id="d3-species-tree">' +
-              '    <div ng-hide="ctrl.response.status">' +
+              '    <div ng-if="!$ctrl.response">' +
               '        <i class="fa fa-spinner fa-spin fa-2x"></i><span class="margin-left-5px">Loading taxonomic tree...</span>' +
               '    </div>' +
-              '    <div ng-show="ctrl.response.status" class="alert alert-danger fade">' +
-              '        <i class="fa fa-exclamation-triangle"></i>Sorry, there was a problem loading the data. Please try again and contact us if the problem persists.' +
-              '    </div>' +
-              '    <div ng-show="ctrl.response.status" class="alert alert-danger fade">' +
+              '    <div ng-if="$ctrl.response.status >= 400" class="alert alert-danger fade">' +
               '        <i class="fa fa-exclamation-triangle"></i>Sorry, there was a problem loading the data. Please try again and contact us if the problem persists.' +
               '    </div>' +
               '</div>'
