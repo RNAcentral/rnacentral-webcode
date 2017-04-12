b0caa2bf6d283cfeb73412546b79485d57ca955e
Boris A. Burkov <vasjaforutube@gmail.com>
2017-04-12 18:52:23 +0100

    #110 Working on publications tab.

--- rnacentral/portal/static/js/sequence.module.js
+++ rnacentral/portal/static/js/sequence.module.js
@@ -48,7 +48,7 @@ var taxonomyComponent = {
     controller: ['$http', '$interpolate',  function($http, $interpolate) {
         var ctrl = this;
 
-        ctrl.$onInit =function() {
+        ctrl.$onInit = function() {
             var d3_species_tree = $('#d3-species-tree');
 
             if (!document.createElement('svg').getAttributeNS) {
@@ -80,6 +80,83 @@ var taxonomyComponent = {
               '</div>'
 };
 
+
+var publicationResourceFactory = function($resource) {
+    return $resource(
+        '/api/v1/rna/:upi/publications?page_size=:page',
+        {upi: '@upi', page: '@page'},
+        {
+            get: {timeout: 5000, isArray: false}
+        }
+    );
+};
+publicationResourceFactory.$inject = ['$resource'];
+
+var publicationsComponent = {
+    bindings: {
+        upi: '<',
+        taxid: '<?'
+    },
+    controller: ['publicationResource', function(publicationResource) {
+        var ctrl = this;
+
+        ctrl.$onInit = function() {
+            var obj = this,
+                target = $('#publications'),
+                load_more_btn_id = '#load-more-publications',
+                template_id = '#handlebars-publications',
+                url = '/api/v1/rna/__URS__/publications?page_size=__PAGE_SIZE__';
+
+            var page_size = 25;
+
+            $.get(url.replace('__URS__', ctrl.upi).replace('__PAGE_SIZE__', page_size), function(data) {
+                insert_content(data);
+                obj.activate_abstract_buttons(target.find('.abstract-btn'));
+            });
+
+            // attach event to the load more button
+            target.off('click').on('click', load_more_btn_id, function(){
+                new_page_size = target.find('li').length + page_size;
+                obj.load_publications(new_page_size);
+            });
+
+            function insert_content(data) {
+                var source = $(template_id).html();
+                var template = Handlebars.compile(source);
+                data.total = data.count;
+                data.count = data.results.length;
+                var wrapper = {
+                    data: data,
+                };
+                target.html(template(wrapper));
+            }
+        };
+    }],
+    template: '<div id="publications">' +
+              '    <h2>Publications <small>{{ $ctrl.publications.count }}</small></h2>' +
+              '    <ol ng-repeat="publication in $ctrl.publications"></ol>' +
+              '        <div class="col-md-8">' +
+              '            <li class="margin-bottom-10px">' +
+              '                {{ publication }}' +
+              '            </li>' +
+              '        </div>' +
+              '    </ol>' +
+              '    <div class="col-md-8">' +
+              '        {{#ifCond count '<' total}}' +
+              '        <small class="text-muted">Displaying {{count}} of {{total}} publications</small>' +
+              '        <br>' +
+              '        <button class="btn btn-default btn-large" id="load-more-publications">Load more</button>' +
+              '        {{/ifCond}}' +
+              '    </div>' +
+              '    <div class="row">' +
+              '        <div ng-if="!$ctrl.response" class="col-md-12">' +
+              '            <i class="fa fa-spinner fa-spin fa-2x"></i>' +
+              '            <span class="margin-left-5px">Loading publications...</span>' +
+              '        </div>' +
+              '    ' +
+              '    </div>' +
+              '</div>'
+};
 
 
 var rnaSequenceController = function($scope, $location, $http, $interpolate, xrefResource, DTOptionsBuilder, DTColumnBuilder) {
@@ -125,8 +202,10 @@ rnaSequenceController.$inject = ['$scope', '$location', '$http', '$interpolate',
 
 angular.module("rnaSequence", ['datatables', 'ngResource'])
     .factory("xrefResource", xrefResourceFactory)
+    .factory("publicationResource", publicationResourceFactory)
     .controller("rnaSequenceController", rnaSequenceController)
     .component("xrefsComponent", xrefsComponent)
-    .component("taxonomyComponent", taxonomyComponent);
+    .component("taxonomyComponent", taxonomyComponent)
+    .component("publicationsComponent", publicationsComponent);
 
 })();
