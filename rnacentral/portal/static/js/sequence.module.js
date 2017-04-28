736f7c4e2a00328d4eed4756d188d1e18780d490
Boris A. Burkov <vasjaforutube@gmail.com>
2017-04-28 13:47:11 +0100

    #110 Abstracted-out a separate abstract component.

--- rnacentral/portal/static/js/sequence.module.js
+++ rnacentral/portal/static/js/sequence.module.js
@@ -98,51 +98,19 @@ var taxonomyComponent = {
 };
 
 
-var publicationResourceFactory = function($resource) {
-    return $resource(
-        '/api/v1/rna/:upi/publications?page_size=:page',
-        {upi: '@upi', page: '@page'},
-        {
-            get: {timeout: 5000, isArray: false}
-        }
-    );
-};
-publicationResourceFactory.$inject = ['$resource'];
-
-
-var publicationsComponent = {
+var abstractComponent = {
     bindings: {
-        upi: '<',
-        taxid: '<?'
+        pubmed_id: '<'
     },
-    controller: ['publicationResource', '$http', '$interpolate', function(publicationResource, $http, $interpolate) {
+    require: {
+        parent: '^publicationsComponent'
+    },
+    controller: ['$http', '$interpolate', function($http, $interpolate) {
         var ctrl = this;
 
-        ctrl.$onInit = function() {
-            ctrl.abstracts = {};
-            ctrl.publications = ctrl.fetchPublications(25);
-        };
-
-        /**
-         * Asynchronously downloads <pageSize> (e.g. 25) publications
-         * on this sequences and stores in ctrl.publications.
-         *
-         * @param {int} [25] pageSize - how many publications to load
-         * @returns {publicationResource promise} - Array-like of publications
-         */
-        ctrl.fetchPublications = function(pageSize) {
-            pageSize = pageSize || 25;
-
-            return publicationResource.get(
-                { upi: this.upi, page_size: pageSize },
-                function(publications) {
-                    // retrieve corresponding abstracts
-                    for (var i=0; i < publications.results.length; i++) {
-                        var pubmed_id = publications.results[i].pubmed_id;
-                        ctrl.fetchAbstract(pubmed_id);
-                    }
-                }
-            );
+        ctrl.$onChanges = function(changes) {
+            console.log(changes);
+            ctrl.fetchAbstract(changes.pubmed_id);
         };
 
         /**
@@ -160,17 +128,60 @@ var publicationsComponent = {
                     $interpolate('http://www.ebi.ac.uk/europepmc/webservices/rest/search?query=ext_id:{{ pubmed_id }}&format=json&resulttype=core')({pubmed_id: pubmed_id})
                 ).then(
                     function(response) {
-                        ctrl.abstracts[pubmed_id] = response.data.resultList.result[0].abstractText;
+                        ctrl.abstract = response.data.resultList.result[0].abstractText;
                     },
                     function(response) {
-                        ctrl.abstracts[pubmed_id] = "Failed to download abstract";
+                        ctrl.abstract = "Failed to download abstract";
                     }
                 );
             }
             else {
-                ctrl.abstracts[pubmed_id] = "Abstract is not available";
+                ctrl.abstract = "Abstract is not available";
                 return null;
             }
+        };
+
+    }],
+    template: '<button class="btn btn-xs btn-default abstract-btn abstract-control" ng-click="abstractVisible = !abstractVisible"><span ng-if="abstractVisible">Hide abstract</span><span ng-if="!abstractVisible">Show abstract</span></button>' +
+              '<div ng-if="abstractVisible" class="abstract-text slide-down">{{ $ctrl.abstracts[publication.pubmed_id] }}</div>'
+};
+
+
+var publicationResourceFactory = function($resource) {
+    return $resource(
+        '/api/v1/rna/:upi/publications?page_size=:page',
+        {upi: '@upi', page: '@page'},
+        {
+            get: {timeout: 5000, isArray: false}
+        }
+    );
+};
+publicationResourceFactory.$inject = ['$resource'];
+
+
+var publicationsComponent = {
+    bindings: {
+        upi: '<',
+        taxid: '<?'
+    },
+    controller: ['publicationResource', function(publicationResource) {
+        var ctrl = this;
+
+        ctrl.$onInit = function() {
+            ctrl.abstracts = {};
+            ctrl.publications = ctrl.fetchPublications(25);
+        };
+
+        /**
+         * Asynchronously downloads <pageSize> (e.g. 25) publications
+         * on this sequences and stores in ctrl.publications.
+         *
+         * @param {int} [25] pageSize - how many publications to load
+         * @returns {publicationResource promise} - Array-like of publications
+         */
+        ctrl.fetchPublications = function(pageSize) {
+            pageSize = pageSize || 25;
+            return publicationResource.get({ upi: this.upi, page_size: pageSize });
         };
 
         ctrl.loadMore = function(pageSize) {
@@ -193,8 +204,7 @@ var publicationsComponent = {
               '                    <span ng-if="publication.pubmed_id">' +
               '                        <a href="http://www.ncbi.nlm.nih.gov/pubmed/{{ publication.pubmed_id }}" class="margin-left-5px">Pubmed</a>' +
               '                        <a ng-if="publication.doi" href="http://dx.doi.org/{{ publication.doi }}" target="_blank" class="abstract-control">Full text</a>' +
-              '                        <button class="btn btn-xs btn-default abstract-btn abstract-control" ng-click="abstractVisible = !abstractVisible"><span ng-if="abstractVisible">Hide abstract</span><span ng-if="!abstractVisible">Show abstract</span></button>' +
-              '                        <div ng-if="abstractVisible" class="abstract-text slide-down">{{ $ctrl.abstracts[publication.pubmed_id] }}</div>' +
+              '                        <abstract-component pubmed_id="publication.pubmed_id"></abstract-component>' +
               '                    </span>' +
               '                  <br>' +
               '                  <a href="/search?q=pub_id:&#34;{{ publication.pubmed_id }}&#34;" class="margin-left-5px"><i class="fa fa-search"></i> Find other sequences from this reference</a>' +
@@ -325,6 +335,7 @@ angular.module("rnaSequence", ['datatables', 'ngResource', 'ngAnimate'])
     .controller("rnaSequenceController", rnaSequenceController)
     .component("xrefsComponent", xrefsComponent)
     .component("taxonomyComponent", taxonomyComponent)
-    .component("publicationsComponent", publicationsComponent);
+    .component("publicationsComponent", publicationsComponent)
+    .component("abstractComponent", abstractComponent);
 
 })();
