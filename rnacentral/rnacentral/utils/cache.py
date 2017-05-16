7e8041567c9b130d6621872deb0f96c90ba44862
Boris A. Burkov <vasjaforutube@gmail.com>
2017-05-16 16:42:00 +0100

    #114 Added the standard EBI license to cache.py.

--- rnacentral/rnacentral/utils/cache.py
+++ rnacentral/rnacentral/utils/cache.py
@@ -1,3 +1,15 @@
+"""
+Copyright [2009-2017] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 "File-based cache backend"
 import errno
 import glob
@@ -18,6 +30,7 @@ except ImportError:
     import pickle
 
 
+# TODO: when migrate to a newer version of django, don't forget to copy-paste newer version of SitemapsCache code here
 class SitemapsCache(BaseCache):
     cache_suffix = '.djcache'
 
