3d3824dc60fd5fa5af620306d78150f50ac74fc4
Anton Petrov <apetrov@ebi.ac.uk>
2017-05-17 12:04:47 +0100

    Fix missing import in `url.py`

--- rnacentral/portal/urls.py
+++ rnacentral/portal/urls.py
@@ -11,6 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+from django.conf import settings
 from django.conf.urls import patterns, url
 
 from portal import views
