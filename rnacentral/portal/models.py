d74161bc828175fb1ffad57dbd4b6f0c12f75f4d
Boris A. Burkov <burkov@ebi.ac.uk>
2017-08-21 12:36:22 +0100

    #210 Minor comments, explaining model fields.

--- rnacentral/portal/models.py
+++ rnacentral/portal/models.py
@@ -734,11 +734,11 @@ class Accession(models.Model):
 class GenomicCoordinates(models.Model):
     accession = models.ForeignKey('Accession', db_column='accession', to_field='accession', related_name='coordinates')
     primary_accession = models.CharField(max_length=50)
-    local_start = models.IntegerField()
-    local_end = models.IntegerField()
+    local_start = models.IntegerField()  # start relative to ENA accession
+    local_end = models.IntegerField()  # stop relative to ENA accession
     chromosome = models.CharField(max_length=50, db_column='name')
-    primary_start = models.IntegerField()
-    primary_end = models.IntegerField()
+    primary_start = models.IntegerField()  # relative to chromosome (which is a chromosome or other top level genome id)
+    primary_end = models.IntegerField()  # relative to chromosome (which is a chromosome or other top level genome id)
     strand = models.IntegerField()
 
     class Meta:
