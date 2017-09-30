2e8538bff6a126fe068de5d37f18fc64e9cba6f0
Boris A. Burkov <vasjaforutube@gmail.com>
2017-10-01 00:05:18 +0100

    #110 Implementing pagination for Xrefs table.

--- rnacentral/portal/static/js/components/sequence/xrefs/xrefs.component.js
+++ rnacentral/portal/static/js/components/sequence/xrefs/xrefs.component.js
@@ -10,65 +10,42 @@ var xrefs = {
     controller: ['routes', '$http', '$interpolate', '$timeout', function(routes, $http, $interpolate, $timeout) {
         var ctrl = this;
 
+        ctrl.onPageChanged = function(page) {
+            if (ctrl.paginateOn == 'client') {
+                ctrl.displayedXrefs = xrefs.slice(ctrl.page*ctrl.pageSize, ctrl.pageSize);
+            }
+            else if (ctrl.paginateOn == 'server') {
+                ctrl.page = page;
+                $http.get(ctrl.dataEndpoint, {params: { page: ctrl.page, page_size: ctrl.pageSize }}).then(
+                    function(response) {
+                        ctrl.displayedXrefs = response.data.results;
+                    },
+                    function(response) {
+                        console.log("failed to download a page");
+                    }
+                )
+            }
+        };
+
         ctrl.serverSidePagination = function(page) {
             ctrl.page = page;
             $http.get(ctrl.dataEndpoint, {params: { page: ctrl.page, page_size: ctrl.pageSize }}).then(
                 function(response) {
                     ctrl.xrefs = response.data.results;
 
-                    $timeout(function() {
-                        $('#annotations-table').DataTable({
-                            "columnDefs": [
-                                { targets: [0, 1, 2], visible: true },
-                                { targets: [3, 4, 5], visible: false}
-                            ], // hide columns, but keep them sortable
-                            "autoWidth": true, // pre-recalculate column widths
-                            "dom": "ftpil", // filter, table, pagination, information, length
-                            //"paginationType": "bootstrap", // requires dataTables.bootstrap.js
-                            "deferRender": false, // defer rendering until necessary
-                            "language": {
-                                "search": "", // don't display the "Search:" bit
-                                "info": "_START_-_END_ of _TOTAL_", // change the informational text
-                                "paginate": {
-                                    "next": "",
-                                    "previous": "",
-                                }
-                            },
-                            "order": [[ 5, "desc" ]], // prioritize entries with genomic coordinates
-                            "lengthMenu": [[5, 10, 20, 50, -1], [5, 10, 20, 50, "All"]],
-                            "initComplete": function(settings, json) {
-                                // some weird scripts sets width smaller than it should be
-                                $("#annotations-table").css("width",  "100%");
+                    ctrl.displayedXrefs = ctrl.xrefs;
 
-                                // modify filter field and move it up
-                                $('.dataTables_filter input').attr('placeholder', 'Filter table')
-                                                             .attr('tabindex', 2)
-                                                             .attr('type', 'search')
-                                                             .addClass('form-control input-sm');
-
-                                $('#annotations-table_filter').appendTo('#annotations-datatables-filter')
-                                                              .addClass('pull-right hidden-xs');
-
-                                // replace angular row counter with datatables one
-                                $('#annotations-datatables-counter').html('');
-                                $('#annotations-table_info').appendTo('#annotations-datatables-counter');
-
-                                // create our own pagination that makes requests to the server side
-                                var totalPages = Math.floor(reponse.data.count, ctrl.pageSize);
-                                var pagination = $('ul').addClass('pagination pagination-sm');
-                                for (var page = 0; page < totalPages; page++) {
-                                    var page = $('li');
-                                    if (page == ctrl.page) page.addClass('active');
-                                    pagination.append(page);
-                                }
-                                pagination.on('click', ctrl.serverSidePagination);
-                                pagination.appendTo('.annotations-datatables-controls');
-                                pagination.addClass('pull-left');
-
-                                $('.table th').css('padding-right','1.5em');
-                            }
-                        });
-                    });
+                    // create our own pagination that makes requests to the server side
+                    var totalPages = Math.floor(reponse.data.count, ctrl.pageSize);
+                    var pagination = $('ul').addClass('pagination pagination-sm');
+                    for (var page = 0; page < totalPages; page++) {
+                        var page = $('li');
+                        if (page == ctrl.page) page.addClass('active');
+                        pagination.append(page);
+                    }
+                    pagination.on('click', ctrl.serverSidePagination);
+                    pagination.appendTo('.annotations-datatables-controls');
+                    pagination.addClass('pull-left');
                 },
                 function(response) {
                     // TODO: display an error message in template!
