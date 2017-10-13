4d583913a56ba24a4b20b417924aef094b3d332b
Blake Sweeney <bsweeney@ebi.ac.uk>
2017-10-13 11:15:36 +0100

    Improve MGI import
    This was missing storing accession and reference level data. Hopefully
    it does a better job now.

--- rnacentral/portal/management/commands/import_mgi.py
+++ rnacentral/portal/management/commands/import_mgi.py
@@ -21,22 +21,25 @@ from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
 
 from portal.models import Xref
+from portal.models import Accession
 from portal.models import Database
 from portal.models import Release
 
 
 class MgiImporter(object):
     def __init__(self):
-        self.database = Database.objects.get(descr='MGI')
+        self.database_name = 'MGI'
+        self.database = Database.objects.get(descr=self.database_name)
         self.release = Release.objects.get(db_id=self.database.id)
 
     def delete_current_data(self):
         Xref.objects.filter(db_id=self.database.id).delete()
+        Accession.objects.filter(database=self.database_name).delete()
+        Reference_map.objects.filter(accession__database=self.database_name).delete()
 
     def save_xref(self, entry):
-        return Xref.create_or_update(
+        Xref.update_or_create(
             db_id=self.database.id,
-            accession_id=entry['accession'],
             created_id=self.release.id,
             last_id=self.release.id,
             upi_id=entry['rnacentral_id'],
@@ -44,9 +47,68 @@ class MgiImporter(object):
             deleted='N',
             timestamp=datetime.now(),
             userstamp='RNACEN',
+            accession_id=entry['accession'],
             version=1,
             taxid=entry['ncbi_taxon_id'],
         )
+
+    def save_accession(self, entry):
+        Accession.create_or_update(
+            accession=entry['accession'],
+            parent_ac=entry['parent_accession'],
+            seq_version=entry['seq_version'],
+            feature_start=entry['feature_location_start'],
+            feature_end=entry['feature_location_end'],
+            feature_name=entry['feature_name'],
+            ordinal=entry['ordinal'],
+            division=entry['division'],
+            description=entry['description'],
+            species=entry['species'],
+            organelle=entry['organelle'],
+            classification=entry['classification'],
+            project=entry['project'],
+            is_composite=entry['is_composite'],
+            non_coding_id=entry['non_coding_id'],
+            database=entry['database'],
+            external_id=entry['external_id'],
+            optional_id=entry['optional_id'],
+            common_name=entry['common_name'],
+            allele=entry['allele'],
+            anticodon=entry['anticodon'],
+            chromosome=entry['chromosome'],
+            experiment=entry['experiment'],
+            function=entry['function'],
+            gene=entry['gene'],
+            gene_synonym=entry['gene_synonym'],
+            inference=gene['inference'],
+            locus_tag=gene['locus_tag'],
+            map=gene['map'],
+            mol_type=entry['mol_type'],
+            ncrna_class=entry['ncrna_class'],
+            note=json.dumps(entry['note_data']),
+            old_locus_tag=entry['old_locus_tag'],
+            operon=entry['operon'],
+            product=entry['product'],
+            psuedogene=entry['psuedogene'],
+            standard_name=entry['standard_name'],
+            db_xref=json.dumps(entry['xref_data']),
+        )
+
+    def save_reference_data(entry):
+        return Reference.update_or_create(
+            authors=entry['authors'],
+            location=entry['location'],
+            title=entry['title'],
+            pubmed=entry['pubmed'],
+            doi=entry['doi'],
+        )
+
+    def save_references(self, entry):
+        for reference in self.save_reference_data(entry):
+            Reference_map.update_or_create(
+                accession_id=entry['accession'],
+                data_id=reference_data.id,
+            )
 
     def __call__(self, filename):
         with open(filename, 'rb') as raw:
@@ -54,7 +116,12 @@ class MgiImporter(object):
 
         self.delete_current_data()
         for entry in data:
+            if not entry['rnacentral_id']:
+                continue
+
             self.save_xref(entry)
+            self.save_accession(entry)
+            self.save_references(entry)
 
 
 class Command(BaseCommand):
@@ -71,7 +138,7 @@ class Command(BaseCommand):
             help='[Required] Path to MGI JSON file with RNAcentral identifiers'),
     )
     # shown with -h, --help
-    help = ('Import HGNC cross-references into RNAcentral database')
+    help = ('Import all MGI data into RNAcentral database')
 
     def handle(self, *args, **options):
         """
