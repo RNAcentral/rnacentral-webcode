67649a4e1eb65f8eef64f96861d67d439e662ce9
Boris A. Burkov <burkov@ebi.ac.uk>
2017-10-20 17:15:03 +0100

    #110 Working on expert-database page.

--- rnacentral/portal/static/js/components/expert-database/expert-database.module.js
+++ rnacentral/portal/static/js/components/expert-database/expert-database.module.js
@@ -1,13 +1,31 @@
-var expertDatabaseController = function($scope, $location, $window, $rootScope, routes) {
-    $http.get(routes.expertDbsApi).then(
+var expertDatabaseController = function($scope, $location, $window, $rootScope, $http, routes) {
+    // initialize $scope variables
+    $scope.dbName = $location.path().split('/')[2];
+    $scope.expertDb = null;
+    $scope.error = false;
+    $scope.routes = routes;  // expose routes in template for error message
+
+    // download expertDbs json from server, pick appropriate database from it
+    $http.get(routes.expertDbsApi()).then(
         function(response) {
-            $scope.expertDb = reponse.data;
+            // find this particular database in array, returned from server
+            for (var i = 0; i < response.data.length; i++) {
+                if (response.data[i].label === $scope.dbName) {
+                    $scope.expertDb = response.data[i];
+                    break
+                }
+                if (i === response.data.length - 1) {
+                    console.log("Something's wrong, I can't find database '" + $scope.dbName + "' in our databases list");
+                }
+            }
         },
-        function(response) {}
+        function(response) {
+            $scope.error = true;
+        }
     )
 };
 
-expertDatabaseController.$inject = ['$scope', '$location', '$window', '$rootScope', 'routes'];
+expertDatabaseController.$inject = ['$scope', '$location', '$window', '$rootScope', '$http', 'routes'];
 
 
 angular.module("expertDatabase", ['ngResource', 'ngAnimate', 'ngSanitize', 'ui.bootstrap'])
