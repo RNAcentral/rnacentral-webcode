58b3b66abb3ad9923fbe870b6ec06260cb0e633c
Anton Petrov <apetrov@ebi.ac.uk>
2017-10-27 14:39:34 +0100

    Add secondary structure angular component

--- rnacentral/portal/static/js/components/routes.service.js
+++ rnacentral/portal/static/js/components/routes.service.js
@@ -16,7 +16,6 @@ angular.module("rnacentralApp").service('routes', ['$interpolate', function($int
         expertDbStatsApi: $interpolate('/api/v1/expert-db-stats/{{ expertDbName }}'),
         textSearch: $interpolate('search'),
         expertDbLogo: $interpolate('/static/img/expert-db-logos/{{ expertDbName }}.png'),
+        apiSecondaryStructuresView: $interpolate('/api/v1/rna/{{ upi }}/2d/{{ taxid }}'),
     };
 }]);
-
-
