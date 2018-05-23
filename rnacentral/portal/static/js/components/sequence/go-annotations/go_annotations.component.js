b43543fef9156ebb482738e01cc32dabe9c4e103
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-05-23 09:06:21 +0100

    Apply ordering to GO annotations
    The annotations should be sorted so involved_in is higher than other
    terms. This applies an ordering to make the table more useful.

--- rnacentral/portal/static/js/components/sequence/go-annotations/go_annotations.component.js
+++ rnacentral/portal/static/js/components/sequence/go-annotations/go_annotations.component.js
@@ -18,10 +18,29 @@ var go_annotations = {
             "parkinsonsuk-ucl": "http://www.ucl.ac.uk/functional-gene-annotation/neurological",
         };
 
+        var qualiferOrdering = {
+            "involved_in": 0,
+            "enables": 1, 
+            "contributes_to": 2,
+            "colocalizes_with": 3,
+            "part_of": 4,
+            "__unknown__": 5,
+            
+            "indexOf": function(annotation) {
+                if (this.hasOwnProperty(annotation.qualifier)) {
+                    return this[annotation.qualifier];
+                }
+                return this.__unknown__;
+            }
+        };
+
         ctrl.$onInit = function() {
+            ctrl.chart_data = '';
             ctrl.fetchGoTerms().then(
                 function(response) {
                     ctrl.go_annotations = response.data;
+                    ctrl.go_annotations.sort(ctrl.compareAnnotations);
+
                     ctrl.go_annotations.forEach(function(annotation) {
                         var key = annotation.assigned_by.toLowerCase();
                         annotation.assigned_url = sourceUrls[key];
@@ -36,6 +55,12 @@ var go_annotations = {
             );
         };
 
+        ctrl.compareAnnotations = function(first, second) {
+            // var index1 = qualiferOrdering.hasOwnProperty(first.qualifier) ? qualiferOrdering[first.qualifier] : qualiferOrdering.__unknown__;
+            // var index2 = qualiferOrdering.hasOwnProperty(second.qualifier) ? qualiferOrdering[second.qualifier] : qualiferOrdering.__unknown__;
+            return qualiferOrdering.indexOf(first) - qualiferOrdering.indexOf(second);
+        };
+
         ctrl.fetchGoTerms = function() {
             return $http.get(
                 routes.apiGoTermsView({ upi: ctrl.upi, taxid: ctrl.taxid }),
@@ -47,7 +72,6 @@ var go_annotations = {
             var ontology = term_id.split(':')[0].toLowerCase();
             var png = routes.quickGoChart({ ontology: ontology, term_ids: term_id });
             ctrl.modal_status = 'loading';
-            ctrl.chart_data = '';
             $('#go-annotation-chart-modal').modal();
 
             $http.get(png, { timeout: 5000 }).then(
