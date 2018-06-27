acbf5273c7b5b0489004088c6dcaa92827655f74
Boris Burkov <vasjaforutube@gmail.com>
2018-06-27 14:47:23 +0100

    383 refresh vdbs (#406)
    *  #383 Trying to fix shell script invocation from jenkinsfile.
    
    *  #383 Echo pwd.
    
    *  #383 Print ls.
    
    *  #383 Cd to jenkins directory.
    
    *  #383 ./script.sh -> bash script.sh
    
    *  #383 Fixed path in the script.
    
    *  #383 Double quotes for variable interpolation.
    
    *  #383 Trying to pass password in to bash script.
    
    *  #383 Got rid of script, trying inline again.
    
    *  #383 Removed cd.
    
    *  #383 Hopefully fixed cd location.
    
    *  #383 Trying to interpolate password for fabric.
    
    *  #383 Removed unused withCredentials.
    
    *  #383 Commented-out password saving for testing.
    
    *  #383 Trying to fix saving of password and keyfile.
    
    *  #383 Commented-out password saving again.
    
    *  #383 Renamed password variable.
    
    *  #383 Trying to echo-print VMPASS.
    
    *  #383 Trying to echo-print a variable.
    
    *  #383 Trying to interpolate variable harder.
    
    *  #383 Even harder...
    
    *  #383 Trying to output the variables.
    
    *  #383 Links to nice pieces of documentation.
    
    *  #383 Let's interpolate vmpass and keyfile.
    
    *  #383 Scripted -> declarative pipeline.
    
    *  #383 Debugging declarative credentials.
    
    *  #383 Added missing steps.
    
    *  #383 Fixes in declarative syntax.
    
    *  #383 Trying to fix user/password.
    
    *  #383 Typo fix.
    
    *  #383 Checking password.
    
    *  #383 Fixed pg task in fabfile.
    
    *  #383 Write keyfile to the disk.
    
    *  #383 Try passing password through a file.
    
    *  #383 We don't need cat to put password into a file.
    
    *  #383 Added missing echo command.
    
    *  #383 Fixed vmpass location.
    
    *  #383 Trying to assign Jenkins password variable to bash variable.
    
    *  #383 Replaced triple double with triple single quotes.
    
    *  #383 Don't mess with paramiko - pass password as a param.
    
    *  #383 Created refresh_vdbs.py script.
    
    *  #383 Added refresh_vdbs.py invocation to jenkinsfile.
    
    *  #383 Fixed cd location.
    
    *  #383 Simplifications in refresh_vdbs.sh.
    
    *  #383 Trying to pass password through a file.
    
    *  #383 Added strip() to password file.
    
    *  #383 Reverted back to fabric (hopefully will work with correct password).
    
    *  #383 Fixed path to fabfile.
    
    *  #383 Got rid of unused files.
    
    *  #383 Unified jenkinsfile for refreshing vdbs.
    
    *  #383 Splitted the pipeline into stages.
    
    *  #383 Fixed missing parentheses.
    
    *  #383 Added missing steps to checkout scm.
    
    *  #383 Explicit checkout scm is not mandatory in a declarative pipeline.
    
    *  #383 Added slack hooks to the pipeline.

--- jenkins/refresh_vdbs.sh
+++
@@ -1,5 +0,0 @@
-cd rnacentral
-SNAPSHOT="2018-06-25 10:13"
-echo $SNAPSHOT
-echo $1
-fab pg --password=$1 refresh_pg:snapshot='$SNAPSHOT'
--- rnacentral/fabfile.py
+++ rnacentral/fabfile.py
@@ -29,6 +29,7 @@ For more options, run `fab help`.
 
 import os
 import json
+import re
 import requests
 
 from fabric.api import cd, env, lcd, local, prefix, run, warn_only
@@ -252,8 +253,8 @@ def test(base_url='http://localhost:8000/'):
     """
     with env.cd(settings.PROJECT_PATH):
         # env.run('python rnacentral/apiv1/tests.py --base_url=%s' % base_url)
-        env.run('python rnacentral/portal/tests/selenium_tests.py --base_url %s --driver=phantomjs' % base_url) # pylint: disable=C0301
-        env.run('python rnacentral/apiv1/search/sequence/tests.py --base_url %s' % base_url) # pylint: disable=C0301
+        env.run('python rnacentral/portal/tests/selenium_tests.py --base_url %s --driver=phantomjs' % base_url)  # pylint: disable=C0301
+        env.run('python rnacentral/apiv1/search/sequence/tests.py --base_url %s' % base_url)  # pylint: disable=C0301
 
 
 # VDBS refresh-related code
@@ -275,7 +276,7 @@ def pg():
     """
     Configures environment variables for running commands on pg, e.g.:
 
-    fab pg --password=mytopsecretpassword refresh_pg:snapshot='2018-06-25 10:13'
+    fab pg --password=mytopsecretpassword refresh_pg
     """
     env.hosts = ['pg-001.ebi.ac.uk']
     env.user = 'burkov'
@@ -287,8 +288,14 @@ def refresh_fb1():
     snapshot = env.run("sudo -u dxrnacen /nfs/dbtools/delphix/postgres/ebi_create_snapshot.sh -s pgsql-hxvm-038.ebi.ac.uk | tail -1")
     env.run("sudo -u dxrnacen /nfs/dbtools/delphix/postgres/ebi_refresh_vdb.sh -d pgsql-dlvm-010.ebi.ac.uk -S '%s'" % snapshot)
     env.run("sudo -u dxrnacen /nfs/dbtools/delphix/postgres/ebi_push_replication.sh -s pgsql-hxvm-038.ebi.ac.uk")
-    print(snapshot)
+
+    slack("Refreshed FB1 database from '%s' snapshot" % snapshot)
 
 
-def refresh_pg(snapshot):
+def refresh_pg():
+    snapshot = env.run("sudo -u dxrnacen /nfs/dbtools/delphix/postgres/ebi_list_snapshots.sh -d pgsql-dlvmpub1-010.ebi.ac.uk | tail -1")
+    # bash curses about absence of /home directory, thus we have to filter out snapshot
+    snapshot = re.search("\d\d\d\d\-\d\d\-\d\d\s\d\d\:\d\d$", snapshot).group(0)  # e.g. 2018-06-26 11:16
     env.run("sudo -u dxrnacen /nfs/dbtools/delphix/postgres/ebi_refresh_vdb.sh -d pgsql-dlvmpub1-010.ebi.ac.uk -S '%s'" % snapshot)
+
+    slack("Refreshed PG database from '%s' snapshot" % snapshot)
