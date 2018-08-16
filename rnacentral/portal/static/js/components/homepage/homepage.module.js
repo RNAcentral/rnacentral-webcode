9b92ed69a978192c4c5811911d9f7f1af0a5e5d1
Boris A. Burkov <burkov@ebi.ac.uk>
2018-08-16 17:17:09 +0100

    Re-used useCases service on the front page.

--- rnacentral/portal/static/js/components/homepage/homepage.module.js
+++ rnacentral/portal/static/js/components/homepage/homepage.module.js
@@ -1,43 +1,8 @@
-var HomepageController = function($scope) {
-    $scope.rainbowTroutPublication = {
-        "title": "Genome-Wide Discovery of Long Non-Coding RNAs in Rainbow Trout.",
-        "authors": ["Al-Tobasei R", "Paneru B", "Salem M"],
-        "publication": "PLoS One. 2016 Feb 19;11(2):e0148940",
-        "pubmed_id": 26895175,
-        "doi": "10.1371/journal.pone.0148940",
-        "pub_id": ""
-    };
-
-    $scope.bovineLeukemiaPublication = {
-        "title": "Characterization of novel Bovine Leukemia Virus (BLV) antisense transcripts by deep sequencing reveals constitutive expression in tumors and transcriptional interaction with viral microRNAs.",
-        "authors": ["Durkin K", "Rosewick N", "Artesi M", "Hahaut V", "Griebel P", "Arsic N", "Burny A", "Georges M", "Van den Broeke A"],
-        "publication": "Retrovirology. 2016 May 3;13(1):33",
-        "pubmed_id": 27141823,
-        "doi": "10.1186/s12977-016-0267-8",
-        "pub_id": ""
-    };
-
-    $scope.expandingHorizonsPublication = {
-        "title": "Expanding the horizons of microRNA bioinformatics.",
-        "authors": ["Huntley R", 'Kramarz B', 'Sawford T', 'Umrao Z', 'Kalea A', 'Acquaah V', 'Martin MJ', 'Mayr M', 'Lovering RC'],
-        "publication": "RNA. 2018 Aug;24(8):1005-1017",
-        "pubmed_id": 29871895,
-        "doi": "doi: 10.1261/rna.065565.118",
-        "pub_id": ""
-    };
-
-    $scope.secondaryStructurePredictionPublication = {
-        "title": "Forna (force-directed RNA): Simple and effective online RNA secondary structure diagrams.",
-        "authors": ["Kerpedjiev P", "Hammer S", "Hofacker IL"],
-        "publication": "Bioinformatics. 2015 Oct 15;31(20):3377-9",
-        "pubmed_id": 26099263,
-        "doi": "doi: 10.1093/bioinformatics/btv372",
-        "pub_id": ""
-    };
+var HomepageController = function($scope, useCases) {
+    $scope.useCases = useCases;  // expose useCases to the templates
 };
+HomepageController.$inject = ['$scope', 'useCases'];
 
-HomepageController.$inject = ['$scope'];
 
-
-angular.module("homepage", ['ngResource', 'ngAnimate', 'ngSanitize', 'ui.bootstrap', 'routes', 'rnaSequence'])
+angular.module("homepage", ['ngResource', 'ngAnimate', 'ngSanitize', 'ui.bootstrap', 'routes', 'useCases', 'rnaSequence'])
     .controller("HomepageController", HomepageController);
