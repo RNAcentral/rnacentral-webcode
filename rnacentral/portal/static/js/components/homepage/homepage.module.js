76721ce2e5748b441167272d69ce87bace498a31
Boris A. Burkov <burkov@ebi.ac.uk>
2018-08-16 17:49:22 +0100

    generateRandomExamples() done.

--- rnacentral/portal/static/js/components/homepage/homepage.module.js
+++ rnacentral/portal/static/js/components/homepage/homepage.module.js
@@ -1,5 +1,31 @@
 var HomepageController = function($scope, useCases) {
     $scope.useCases = useCases;  // expose useCases to the templates
+
+    /**
+     * Generate one example per useCases category
+     * @param {Object} useCases
+     * @returns {Object} - an object: { <categoryName>: <useCase> }, e.g.:
+     *   {
+     *     "reference-ncrna-set": [Object object],
+     *     "literature-curation": [Object object],
+     *     "programmatic-information-retrieval": [Object object]
+     *   }
+     */
+    function generateRandomExamples(useCases) {
+        // generate a non-redundant list of categories
+        var categories = [...new Set(Object.values(useCases).map(value => value.category))];
+
+        var output = []
+        categories.forEach(function(category) {
+            var categoryMembers = Object.values(useCases).filter(useCase => useCase.category === category);
+            var randomIndex = Math.floor(Math.random() * categoryMembers.length);
+            output.push(categoryMembers[randomIndex]);
+        });
+
+        return output
+    };
+
+    $scope.randomExamples = generateRandomExamples(useCases);
 };
 HomepageController.$inject = ['$scope', 'useCases'];
 
