979ee746d591dc8a07366d30e27a44cc2329b680
Boris A. Burkov <burkov@ebi.ac.uk>
2018-09-04 12:43:49 +0100

    Partially fixed urls and populated distinctFeatures.

--- rnacentral/portal/static/js/components/sequence/crs/crs.component.js
+++ rnacentral/portal/static/js/components/sequence/crs/crs.component.js
@@ -13,12 +13,22 @@ var crs = {
             ctrl.distinctFeatures = [];
 
             // aggregate features with same id and different locations
+            ctrl.features.forEach(function(feature) {
+                var featureClone = ctrl.distinctFeatures.find(function(el) { return el.metadata.crs_id === feature.metadata.crs_id });
+                if (!featureClone) {
+                    featureClone = JSON.parse(JSON.stringify(feature));
+                    featureClone.locations = [{ start: feature.start, stop: feature.stop, chromosome: feature.chromosome }];
+                    ctrl.distinctFeatures.push(featureClone);
+                } else {
+                    featureClone.locations.push({ start: feature.start, stop: feature.stop, chromosome: feature.chromosome });
+                }
+            });
 
             // for each distinct feature, add links to rth page, secondary structure and alignment
             ctrl.distinctFeatures.forEach(function(feature) {
-                feature.rthPageUrl = "";
-                feature.secondaryStructureUrl = "";
-                feature.alignmentUrl = "";
+                feature.rthPageUrl = $interpolate("https://rth.dk/resources/rnannotator/crs/vert/pages/cmf.data.collection.openallmenus.php?crs={{ crs_id }}")({ crs_id: feature.metadata.crs_id });
+                feature.secondaryStructureUrl = $interpolate("https://rth.dk/resources/rnannotator/crs/vert/data/figure/structure_cons/hg38_17way/M1/{{ crs_id }}_ext_liftover_17way.ss.png")({ crs_id: feature.metadata.crs_id });
+                feature.alignmentUrl = $interpolate("https://rth.dk/resources/rnannotator/crs/vert/data/figure/alignment/hg38_17way/M1/{{ crs_id }}_ext_liftover_17way.aln.png")({ crs_id: feature.metadata.crs_id });
             });
 
 
