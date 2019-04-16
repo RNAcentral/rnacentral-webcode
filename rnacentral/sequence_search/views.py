f0b5b4fde9cdcf39caa47c120c9e68fc94f61315
Boris A. Burkov <burkov@ebi.ac.uk>
2019-04-16 12:46:51 +0100

    Added proxy settings for requests in sequence_search.

--- rnacentral/sequence_search/views.py
+++ rnacentral/sequence_search/views.py
@@ -13,6 +13,7 @@ limitations under the License.
 
 import requests
 
+from django.conf import settings
 from django.views.decorators.cache import never_cache
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
@@ -21,13 +22,30 @@ from rest_framework.permissions import AllowAny
 from .settings import SEQUENCE_SEARCH_ENDPOINT
 
 
+if settings.ENVIRONMENT == 'DEV':
+    proxies = None
+elif settings.ENVIRONMENT == 'HX' or settings.ENVIRONMENT == 'OY':
+    proxies = {
+        'http': 'http://hx-wwwcache.ebi.ac.uk:3128',
+        'https': 'http://hx-wwwcache.ebi.ac.uk:3128'
+    }
+elif settings.ENVIRONMENT == 'PG':
+    proxies = {
+        'http': 'http://pg-wwwcache.ebi.ac.uk:3128',
+        'https': 'http://pg-wwwcache.ebi.ac.uk:3128'
+    }
+
+
 @never_cache
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def submit_job(request):
     """Submit a job to sequence search service."""
     url = SEQUENCE_SEARCH_ENDPOINT + '/api/submit-job'
-    response = requests.post(url, json=request.data)
+    if proxies:
+        response = requests.post(url, json=request.data, proxies=proxies)
+    else:
+        response = requests.post(url, json=request.data)
     return Response(response.json(), status=response.status_code)
 
 
@@ -37,7 +55,10 @@ def submit_job(request):
 def job_status(request, job_id):
     """Displays status of a job."""
     url = SEQUENCE_SEARCH_ENDPOINT + '/api/job-status/' + job_id
-    response = requests.get(url)
+    if proxies:
+        response = requests.get(url, proxies=proxies)
+    else:
+        response = requests.get(url)
     return Response(response.json(), status=response.status_code)
 
 
@@ -47,5 +68,8 @@ def job_status(request, job_id):
 def job_results(request, job_id):
     """Displays results of a finished job."""
     url = SEQUENCE_SEARCH_ENDPOINT + '/api/facets-search/' + job_id
-    response = requests.get(url, params=request.query_params)
+    if proxies:
+        response = requests.get(url, params=request.query_params, proxies=proxies)
+    else:
+        response = requests.get(url, params=request.query_params)
     return Response(response.json(), status=response.status_code)
