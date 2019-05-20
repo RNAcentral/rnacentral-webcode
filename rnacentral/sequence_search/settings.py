056a89a30e8733e28fb667ac4c411c8e89d82ae7
Anton Petrov <apetrov@ebi.ac.uk>
2019-05-20 10:42:29 +0100

    Use local search running with docker-compose

--- rnacentral/sequence_search/settings.py
+++ rnacentral/sequence_search/settings.py
@@ -11,9 +11,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-SEQUENCE_SEARCH_ENDPOINT = 'http://193.62.55.45' # running remotely with a proxy
-# SEQUENCE_SEARCH_ENDPOINT = 'http://localhost:8002' # running locally
-# SEQUENCE_SEARCH_ENDPOINT = 'http://193.62.55.80:8002' # running remotely without a proxy
+# SEQUENCE_SEARCH_ENDPOINT = 'http://193.62.55.45' # running remotely with a proxy
+# SEQUENCE_SEARCH_ENDPOINT = 'http://193.62.55.123:8002' # running remotely without a proxy
+# SEQUENCE_SEARCH_ENDPOINT = 'http://193.62.55.44:8002' # running remotely without a proxy
+SEQUENCE_SEARCH_ENDPOINT = 'http://host.docker.internal:8002' # running locally
 
 # minimum query sequence length
 MIN_LENGTH = 10
