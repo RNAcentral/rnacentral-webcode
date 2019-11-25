7f2e4d7b1b4e6f19ab6198269dfb2d96d4247d6d
carlosribas <caduribas@gmail.com>
2019-11-25 09:33:25 +0000

    Increase maximum run time

--- rnacentral/export/settings.py
+++ rnacentral/export/settings.py
@@ -18,7 +18,7 @@ import os
 EXPIRATION = 60*60*24*7 # seconds
 
 # maximum time to run the job
-MAX_RUN_TIME = 60*60*2 # seconds
+MAX_RUN_TIME = 60*60*12  # seconds
 
 # maximum number of entries that can be paginated over in EBI search
 MAX_OUTPUT = 250000
