ac03e92d73241cb50f9851c7cd30430da515e9b7
carlosribas <caduribas@gmail.com>
2019-12-20 13:56:13 +0000

    Fix Sequence Search import

--- rnacentral/sequence_search/views.py
+++ rnacentral/sequence_search/views.py
@@ -21,7 +21,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
 try:
-    from rnacentral.local_settings import SEQUENCE_SEARCH_ENDPOINT
+    from rnacentral.sequence_search_endpoints import SEQUENCE_SEARCH_ENDPOINT
 except ImportError:
     SEQUENCE_SEARCH_ENDPOINT = 'https://search.rnacentral.org'
 
