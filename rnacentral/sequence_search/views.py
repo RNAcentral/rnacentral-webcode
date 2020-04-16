e9b2490bf5156e15aebc4721f86e6ce8b5891ca7
carlosribas <caduribas@gmail.com>
2020-04-16 19:09:37 +0100

    Use the UKCloud IP address

--- rnacentral/sequence_search/views.py
+++ rnacentral/sequence_search/views.py
@@ -20,10 +20,11 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
-try:
-    from rnacentral.sequence_search_endpoints import SEQUENCE_SEARCH_ENDPOINT
-except ImportError:
-    SEQUENCE_SEARCH_ENDPOINT = 'https://search.rnacentral.org'
+# try:
+#     from rnacentral.sequence_search_endpoints import SEQUENCE_SEARCH_ENDPOINT
+# except ImportError:
+#     SEQUENCE_SEARCH_ENDPOINT = 'https://search.rnacentral.org'
+SEQUENCE_SEARCH_ENDPOINT = 'http://51.179.208.80:8002'
 
 if settings.ENVIRONMENT == 'DEV':
     proxies = None
