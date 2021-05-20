730b536fc663f8292d93fef185f18331efb9b3b8
carlosribas <caduribas@gmail.com>
2021-05-20 13:43:21 +0100

    Create the local_settings file to be used by GitHub Actions

---
+++ rnacentral/rnacentral/unit_test_local_settings.py
@@ -0,0 +1,12 @@
+ENVIRONMENT = 'DEV'
+COMPRESS_ENABLED = False
+
+RQ_QUEUES = {
+    'default': {
+        'HOST': 'localhost',
+        'PORT': 8051,
+        'DB': 0,
+        'DEFAULT_TIMEOUT': 360,
+        'REMOTE_SERVER': None,
+    },
+}
