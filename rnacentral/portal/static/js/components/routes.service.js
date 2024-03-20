c74025d71c5494b1bd5e208d873999eecd114ff0
carlosribas <caduribas@gmail.com>
2024-03-20 12:22:55 +0000

    Fix the display of an RNA table

--- rnacentral/portal/static/js/components/routes.service.js
+++ rnacentral/portal/static/js/components/routes.service.js
@@ -50,6 +50,13 @@ angular.module("routes", []).service('routes', ['$interpolate', function($interp
             '&format=json' +
             '&sort=boost:descending'
         ,
+        ebiDbSequences:
+            '{{ ebiBaseUrl }}' +
+            '?query={{ dbQuery }}' +
+            '&fields=description,length' +
+            '&size=1000' +
+            '&format=json'
+        ,
         ebiAutocomplete: 'http://www.ebi.ac.uk/ebisearch/ws/rest/RNAcentral/autocomplete?term={{ query }}&format=json',
         apiGoTermsView: '/api/v1/rna/{{ upi }}/go-annotations/{{ taxid }}',
         quickGoSummaryPage: 'https://www.ebi.ac.uk/QuickGO/term/{{ term_id }}',
