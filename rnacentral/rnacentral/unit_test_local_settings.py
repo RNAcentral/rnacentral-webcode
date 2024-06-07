cf79ae315600ef3eba026f0ad57ede23d934d20a
carlosribas <caduribas@gmail.com>
2024-06-07 15:35:42 +0100

    Add data to access public DB

--- rnacentral/rnacentral/unit_test_local_settings.py
+++ rnacentral/rnacentral/unit_test_local_settings.py
@@ -1,4 +1,4 @@
-# File created to be used by GitHub Action during the execution of unit tests.
+# File created to be used by GitLab during the execution of unit tests.
 # This file is not used in production.
 
 ENVIRONMENT = "DEV"
@@ -12,4 +12,16 @@ RQ_QUEUES = {
         "DEFAULT_TIMEOUT": 360,
         "REMOTE_SERVER": None,
     },
+}
+
+# Public database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "pfmegrnargs",
+        "USER": "reader",
+        "PASSWORD": "NWDMCE5xdipIjRrp",
+        "HOST": "pgsql-hhvm-001.ebi.ac.uk",  # inside campus
+        "PORT": "5432",
+    }
 }
