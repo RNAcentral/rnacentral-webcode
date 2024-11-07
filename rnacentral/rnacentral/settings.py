a74b37e01d8f79fefb3c8b65ebbe11e2275969fa
carlosribas <caduribas@gmail.com>
2024-11-07 23:13:44 +0000

    Update STATIC_ROOT

--- rnacentral/rnacentral/settings.py
+++ rnacentral/rnacentral/settings.py
@@ -87,7 +87,7 @@ SITEMAPS_URL = "/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = "/srv/rnacentral/static"
+STATIC_ROOT = "/srv/rnacentral/static/"
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
