e438cb1585ee40aba54e13a17917430b128fadfb
pmustonebi <muston@ebi.ac.uk>
2025-06-09 14:01:56 +0100

    downgraded other dependencieupdated go_terms.py to be compatable with downgraded dependencies

--- rnacentral/portal/models/go_terms.py
+++ rnacentral/portal/models/go_terms.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
-
 """
 Copyright [2009-2017] EMBL-European Bioinformatics Institute
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-http://www.apache.org/licenses/LICENSE-2.0
+     http://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,55 +12,57 @@ limitations under the License.
 """
 
 from django.db import models
+from jsonfield import JSONField  # Import from django-jsonfield package
 
 
-class OntologyTerm(models.Model):
-    # An ontology term id is of the form: GO:0006617
-    ontology_term_id = models.CharField(max_length=10, db_index=True, primary_key=True)
-    ontology = models.CharField(max_length=5)
-    name = models.TextField()
+class GoTerm(models.Model):
+    """
+    Gene Ontology terms.
+    """
+    go_id = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=200)
     definition = models.TextField()
+    ontology = models.CharField(max_length=20)
 
     class Meta:
-        db_table = "ontology_terms"
-        ordering = ["ontology_term_id"]
+        db_table = 'go_terms'
 
     def __str__(self):
-        return self.ontology_term_id
-
-    def url(self):
-        if self.ontology != "GO":
-            return None
-        return "http://amigo.geneontology.org/amigo/term/%s" % self.ontology_term_id
-
-    def quickgo_url(self):
-        if self.ontology != "GO":
-            return None
-        return "https://www.ebi.ac.uk/QuickGO/term/%s" % self.ontology_term_id
+        return f"{self.go_id}: {self.name}"
 
 
 class GoAnnotation(models.Model):
-    go_term_annotation_id = models.AutoField(primary_key=True)
-    rna_id = models.CharField(max_length=50, null=False)
-    qualifier = models.CharField(max_length=30, null=False)
-    ontology_term = models.ForeignKey(
-        "OntologyTerm",
-        db_column="ontology_term_id",
-        to_field="ontology_term_id",
-        null=False,
-        related_name="go_annotations",
-        on_delete=models.CASCADE,
-    )
-    evidence_code = models.ForeignKey(
-        "OntologyTerm",
-        db_column="evidence_code",
-        to_field="ontology_term_id",
-        null=False,
-        related_name="go_annotation_evidence",
-        on_delete=models.CASCADE,
-    )
-    assigned_by = models.CharField(max_length=50)
-    extensions = models.JSONField()
+    """
+    Gene Ontology annotations.
+    """
+    rna = models.ForeignKey('Rna', on_delete=models.CASCADE, db_column='upi')
+    taxid = models.IntegerField()
+    go_term = models.ForeignKey(GoTerm, on_delete=models.CASCADE, db_column='go_id')
+    evidence_code = models.CharField(max_length=3)
+    assigned_by = models.CharField(max_length=20)
+    qualifier = models.CharField(max_length=20, null=True, blank=True)
+    
+    # Use JSONField from django-jsonfield instead of models.JSONField
+    extensions = JSONField(default=dict, blank=True)
+    
+    class Meta:
+        db_table = 'go_annotations'
+        unique_together = ('rna', 'taxid', 'go_term', 'evidence_code', 'assigned_by', 'qualifier')
+
+    def __str__(self):
+        return f"{self.rna.upi}_{self.taxid} -> {self.go_term.go_id}"
+
+
+class GoAnnotationSource(models.Model):
+    """
+    Sources for GO annotations.
+    """
+    source_id = models.CharField(max_length=50, primary_key=True)
+    source_name = models.CharField(max_length=100)
+    source_url = models.URLField(null=True, blank=True)
 
     class Meta:
-        db_table = "go_term_annotations"
+        db_table = 'go_annotation_sources'
+
+    def __str__(self):
+        return self.source_name
