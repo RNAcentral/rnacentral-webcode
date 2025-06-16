f906adc1335353a721ed981960efb204cee37d37
pmustonebi <muston@ebi.ac.uk>
2025-06-16 09:59:06 +0100

    Updated SequenceRegionActive.identity to nullable

--- rnacentral/portal/models/sequence_regions_active.py
+++ rnacentral/portal/models/sequence_regions_active.py
@@ -32,7 +32,7 @@ class SequenceRegionActive(models.Model):
     region_stop = models.IntegerField()
     assembly_id = models.CharField(max_length=255)
     was_mapped = models.BooleanField()
-    identity = models.FloatField()
+    identity = models.FloatField(null=True, blank=True)
     exon_count = models.IntegerField()
 
     class Meta:
