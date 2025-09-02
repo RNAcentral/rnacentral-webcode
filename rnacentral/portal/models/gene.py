955752cafbac7bb7176339d4aeebb625837a0d61
Isaac Jandalala <isaacjandalala@gmail.com>
2025-09-02 12:51:01 +0100

    Add metadata model.

--- rnacentral/portal/models/gene.py
+++ rnacentral/portal/models/gene.py
@@ -4,8 +4,8 @@ from django.db import models
 class Gene(models.Model):
     """ Gene details """
     id = models.AutoField(primary_key=True)
-    name = models.TextField(db_column='public_name', blank=False, null=False)
-    chromosome = models.CharField()
+    name = models.TextField(db_column='public_name')
+    chromosome = models.CharField(max_length=40)
     start = models.IntegerField()
     stop = models.IntegerField()
     strand = models.CharField(max_length=1)
@@ -17,3 +17,13 @@ class Gene(models.Model):
 
     def __str__(self):
         return self.name
+
+class GeneMetadata(models.Model):
+    """ Gene Metadata """
+    id = models.AutoField(primary_key=True, db_column='rnc_gene_metadata_id')
+    gene = models.OneToOneField(Gene, on_delete=models.CASCADE, related_name='metadata', db_column='rnc_gene_id')
+    description = models.TextField()
+    so_rna_type = models.TextField()
+    
+    class Meta:
+        db_table = 'rnc_gene_metadata'
