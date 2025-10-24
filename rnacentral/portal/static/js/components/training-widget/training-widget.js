db2f10e360cad55e7f33cc23a78102797a9bc674
pmustonebi <muston@ebi.ac.uk>
2025-10-24 10:31:12 +0100

    Updated layout

--- rnacentral/portal/static/js/components/training-widget/training-widget.js
+++ rnacentral/portal/static/js/components/training-widget/training-widget.js
@@ -156,49 +156,6 @@
                   )
                 : React.createElement("p", { className: "text-center" }, "No upcoming live training events available.")
             )
-          ),
-          React.createElement("section", { className: "vf-tabs__section", id: "vf-tabs__section--1" },
-            React.createElement("div", { className: "vf-grid vf-grid__col-2" },
-              liveData && liveData.entries && liveData.entries.length > 0
-                ? liveData.entries.map((item, index) =>
-                    React.createElement("div", {
-                      key: index,
-                      className: "vf-summary vf-summary--event"
-                    },
-                      React.createElement("p", { className: "vf-summary__date" }, item.fields.type),
-                      React.createElement("h3", { className: "vf-summary__title" },
-                        React.createElement("a", {
-                          href: item.fields.url ? item.fields.url[0] : '#',
-                          target: "_blank",
-                          rel: "noopener noreferrer",
-                          className: "vf-summary__link"
-                        }, item.fields.title)
-                      ),
-                      React.createElement("div", null,
-                        React.createElement("div", { className: "vf-summary__text" },
-                          formatDesc(item.fields.description ? item.fields.description[0] : '')
-                        ),
-                        React.createElement("div", { className: "vf-summary__location" },
-                          React.createElement("div", { className: "vf-u-margin__top--400" }),
-                          React.createElement("span", null, item.fields.status),
-                          " | ",
-                          React.createElement("span", null,
-                            React.createElement("i", { className: "icon icon-common icon-calendar-alt" }),
-                            " ",
-                            item.fields.date_time_clean
-                          ),
-                          React.createElement("span", null,
-                            " | ",
-                            React.createElement("i", { className: "icon icon-common icon-location" }),
-                            " ",
-                            item.fields.venue === "null" || !item.fields.venue ? "Online" : item.fields.venue
-                          )
-                        )
-                      )
-                    )
-                  )
-                : React.createElement("p", { className: "text-center" }, "No upcoming live training events available.")
-            )
           )
         )
       )
